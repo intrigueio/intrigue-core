6c9f2358af324ec4f47a85af0bda95cefce4c01e
jcran <jcran@pentestify.com>
2015-08-02 08:44:05 -0700

    merge Scan functionality into core-cli

--- core-cli.rb
+++ core-cli.rb
@@ -2,7 +2,7 @@
 require 'thor'
 require 'json'
 require 'rest-client'
-require_relative 'core'
+require 'intrigue'
 
 require 'pry' #DEBUG
 
@@ -12,7 +12,18 @@ class CoreCli < Thor
     super
     $intrigue_basedir = File.dirname(__FILE__)
     @server_uri = "http://127.0.0.1:7777/v1"
+    @sidekiq_uri = "http://127.0.0.1:7777/sidekiq"
     @delim = "#"
+
+    # Connect to Intrigue API
+    @x = Intrigue.new
+  end
+
+  desc "stats", "Get queue stats"
+  def stats
+    stats_hash = JSON.parse(RestClient.get("#{@sidekiq_uri}/stats"))
+    puts "Sidkiq: #{stats_hash["sidekiq"]}"
+    puts "Redis: #{stats_hash["redis"]}"
   end
 
   desc "list", "List all available tasks"
@@ -56,11 +67,30 @@ class CoreCli < Thor
       puts "Exception #{e}"
       return
     end
-
   end
 
-  desc "start_and_background [Task] [Type#Entity] [Option1=Value1#...#...]", "Start a task and return the task id"
-  def start_and_background(task_name,entity,options=nil)
+
+  desc "scan [Type#Entity] [Depth]", "Start a recursive scan. Returns the result"
+  def scan(entity,depth=3,options=nil)
+    entity_hash = _parse_entity entity
+
+    # create an array of results so we can shortcut
+    # anything we already know (and not repeat work) during a scan
+    $results = {}
+
+    _recurse entity_hash, depth.to_i
+
+    # Print results
+    puts "Results:"
+    $results.each do |key,value|
+      puts "#{key}:"
+      puts "#{value["entities"].map{|x| "  #{x["type"]} #{x["attributes"]["name"]}"}.join("\n")}"
+    end
+  end
+
+
+  desc "background [Task] [Type#Entity] [Option1=Value1#...#...]", "Start and background a single task. Returns the ID"
+  def background(task_name,entity,options=nil)
 
     entity_hash = _parse_entity entity
     options_list = _parse_options options
@@ -78,8 +108,8 @@ class CoreCli < Thor
 
 
 
-  desc "start [Task] [Type#Entity] [Option1=Value1#...#...]", "Start a task and return the result"
-  def start(task_name,entity,options=nil)
+  desc "single [Task] [Type#Entity] [Option1=Value1#...#...]", "Start a single task. Returns the result"
+  def single(task_name,entity,options=nil)
 
     entity_hash = _parse_entity entity
     options_list = _parse_options options
@@ -90,7 +120,7 @@ class CoreCli < Thor
 
 
     if task_id == "" # technically a nil is returned , but becomes an empty string
-      puts "[-] Task not started. Unknown Error. Exiting"
+      puts "[-] Task not started. Unknown Error. Exiting."
       return
     end
 
@@ -128,8 +158,12 @@ class CoreCli < Thor
   ###
   ### XXX - rewrite this so it uses the API
   ###
-  desc "load [Task] [File] [Option1=Value1#...#...]", "Load entities from a file and run task"
-  def load(task_name,filename,options=nil)
+  desc "load [Task] [File] [Option1=Value1#...#...]", "Load entities from a file and run task on each of them"
+  def load(task_name,filename,options_string=nil)
+
+    # Load in the main core file for direct access to TaskFactory and the Tasks
+    # This makes this super speedy.
+    require_relative 'core'
 
     lines = File.open(filename,"r").readlines
 
@@ -137,12 +171,12 @@ class CoreCli < Thor
       line.chomp!
 
       entity = _parse_entity line
+      options = _parse_options options_string
 
       payload = {
-        :task => task_name,
-        :entity => entity,
-        #:options => [{:name => "max_length",
-        #              :value => 8 }],
+        "task" => task_name,
+        "entity" => entity,
+        "options" => options,
       }
 
       task_id = SecureRandom.uuid
@@ -157,14 +191,137 @@ class CoreCli < Thor
 
 private
 
+  ###
+  ### Main "workflow" function
+  ###
+  def _recurse(entity, depth)
+
+    # Check for bottom of recursion
+    return if depth <= 0
+
+    # Check for prohibited entity name
+    if entity["attributes"]
+      return if is_prohibited entity
+    end
+
+    if entity["type"] == "IpAddress"
+      ### DNS Reverse Lookup
+      _start_task "dns_lookup_reverse",entity,depth
+      ### Whois
+      _start_task "whois",entity,depth
+      ### Shodan
+      #_start_task "search_shodan",entity,depth
+      ### Scan
+      _start_task "nmap_scan",entity,depth
+      ### Geolocate
+      #_start_task "geolocate_host",entity,depth
+    elsif  entity["type"] == "NetBlock"
+      ### Masscan
+      _start_task "masscan_scan",entity,depth
+    elsif entity["type"] == "DnsRecord"
+      ### DNS Forward Lookup
+      _start_task "dns_lookup_forward",entity,depth
+      ### DNS Subdomain Bruteforce
+      _start_task "dns_brute_sub",entity,depth,[{"name" => "use_file", "value" => "false"}]
+    elsif entity["type"] == "Uri"
+      ### Get SSLCert
+      _start_task "uri_gather_ssl_certificate",entity,depth
+      ### Gather links
+      _start_task "uri_gather_and_analyze_links",entity,depth
+      ### Dirbuster
+      _start_task "uri_dirbuster",entity,depth
+      ## screenshot
+      _start_task "uri_screenshot",entity,depth
+      ### spider
+      _start_task "uri_spider",entity,depth
+    elsif entity["type"] == "String"
+      # Brute TLD
+      _start_task "dns_brute_tld",entity,depth
+    else
+      puts "UNHANDLED: #{entity["type"]} #{entity["attributes"]["name"]}"
+      return
+    end
+  end
+
+  def _start_task(task_name,entity,depth,options=[])
+    puts "Calling #{task_name} on #{entity} with options #{options} at depth #{depth}"
+
+    # and run it
+    result = @x.start task_name, entity, options
+
+    # XXX - Store the results for later lookup, avoid duplication (which should save a ton of time)
+    key = "#{task_name}_#{entity["type"]}_#{entity["attributes"]["name"]}"
+    if $results[key]
+      puts "ALREADY FOUND: #{$results[key]["entity"]["attributes"]["name"]}"
+
+      ###
+      ### TODO find entity and link
+      ###
+      #old_entity = Neography::Node.find ....
+      #node.outgoing(:child) << old_entity
+
+      return
+    else
+      $results["#{task_name}_#{entity["type"]}_#{entity["attributes"]["name"]}"] = result
+    end
+
+    # Get the results and iterate
+    result['entities'].each do |result|
+      puts "NEW ENTITY: #{result["type"]} #{result["attributes"]["name"]}"
+
+      # create a new node
+      #this = Neography::Node.create(
+      #  type: y["type"],
+      #  name: y["attributes"]["name"],
+      #  task_log: y["task_log"] )
+      # store it on the current entity
+      #node.outgoing(:child) << this
+
+      # recurse!
+      _recurse(result, depth-1)
+    end
+
+  end
+
+  # List of prohibited entities - returns true or false
+  def is_prohibited entity
+
+    #puts "Checking is_prohibited #{entity}"
+
+    if entity["type"] == "NetBlock"
+      cidr = entity["attributes"]["name"].split("/").last.to_i
+      return true unless cidr >= 22
+    else
+      return true if (  entity["attributes"]["name"] =~ /google/             ||
+                        entity["attributes"]["name"] =~ /g.co/               ||
+                        entity["attributes"]["name"] =~ /goo.gl/             ||
+                        entity["attributes"]["name"] =~ /android/            ||
+                        entity["attributes"]["name"] =~ /urchin/             ||
+                        entity["attributes"]["name"] =~ /youtube/            ||
+                        entity["attributes"]["name"] =~ /schema.org/         ||
+                        entity["attributes"]["description"] =~ /schema.org/  ||
+                        entity["attributes"]["name"] =~ /microsoft.com/      ||
+                        #entity["attributes"]["name"] =~ /yahoo.com/          ||
+                        entity["attributes"]["name"] =~ /facebook.com/       ||
+                        entity["attributes"]["name"] =~ /cloudfront.net/     ||
+                        entity["attributes"]["name"] =~ /twitter.com/        ||
+                        entity["attributes"]["name"] =~ /w3.org/             ||
+                        entity["attributes"]["name"] =~ /akamai/             ||
+                        entity["attributes"]["name"] =~ /akamaitechnologies/ ||
+                        entity["attributes"]["name"] =~ /amazonaws/          ||
+                        entity["attributes"]["name"] == "feeds2.feedburner.com")
+    end
+  false
+  end
+
   # parse out entity from the cli
   def _parse_entity(entity)
     entity_type = entity.split(@delim).first
     entity_name = entity.split(@delim).last
 
     entity_hash = {
-      :type => entity_type,
-      :attributes => { :name => entity_name}
+      "type" => entity_type,
+      "attributes" => { "name" => entity_name}
     }
   entity_hash
   end
@@ -175,7 +332,7 @@ private
       return [] unless options
 
       options_list = options.split(@delim).map do |option|
-        { :name => option.split("=").first, :value => option.split("=").last }
+        { "name" => option.split("=").first, "value" => option.split("=").last }
       end
   options_list
   end
@@ -183,16 +340,16 @@ private
   def _background(task_name,entity_hash,options_list=[])
 
     payload = {
-      :task => task_name,
-      :options => options_list,
-      :entity => entity_hash
+      "task" => task_name,
+      "options" => options_list,
+      "entity" => entity_hash
     }
 
     ###
     ### Send to the server
     ###
 
-    task_id = RestClient.post "#{@server_uri}/task_runs", payload.to_json, :content_type => "application/json"
+    task_id = RestClient.post "#{@server_uri}/task_runs", payload.to_json, "content_type" => "application/json"
 
     task_id
   end
