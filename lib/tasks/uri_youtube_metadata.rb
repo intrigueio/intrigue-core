20af72f0a0b9032d79cf794d36d50556007c45b8
jcran <jcran@pentestify.com>
2015-09-10 01:56:44 -0700

    add a youtube metadata collection task

---
+++ lib/tasks/uri_youtube_metadata.rb
@@ -0,0 +1,52 @@
+module Intrigue
+class UriYoutubeMetadata < BaseTask
+
+  include Intrigue::Task::Web
+
+  def metadata
+    {
+      :name => "uri_youtube_metadata",
+      :pretty_name => "URI Youtube Metadata",
+      :authors => ["jcran"],
+      :description => "This task downloads metadata, given a youtube video Uri.",
+      :references => [],
+      :allowed_types => ["Uri"],
+      :example_entities => [
+        {"type" => "Uri", "attributes" => { "name" => "https://www.youtube.com/watch?v=ZPr-_21-xGQ" }}
+      ],
+      :allowed_options => [],
+      :created_types =>  ["Info"]
+    }
+  end
+
+  ## Default method, subclasses must override this
+  def run
+    super
+    video_uri = _get_entity_attribute "name"
+
+    unless video_uri =~ /www\.youtube\.com\/watch/
+      @task_log.error "This doesn't appear to be a valid youtube video uri."
+      return nil
+    end
+
+    begin
+      video_json = http_get_body "http://www.youtube.com/oembed?url=#{video_uri}&format=json"
+      attributes = JSON.parse video_json
+      _create_entity "Info", attributes.merge({"name" => "YouTube Video: #{attributes["title"]}"})
+
+      if attributes["author_name"]
+        _create_entity "WebAccount", {
+          "name" => attributes["author_name"], 
+          "domain" => "youtube.com",
+          "uri" => video_uri
+        }
+      end
+
+    rescue JSON::ParserError
+      @task_log.error "Unable to retrieve video metadata"
+    end
+
+  end
+
+end
+end
