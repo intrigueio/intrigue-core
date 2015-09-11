1880f6cb12aadbf894351095990b68e7b0505c47
jcran <jcran@pentestify.com>
2015-09-10 22:18:52 -0700

    cleanup, handle a few more input cases

--- lib/tasks/phone_number_carrier_lookup.rb
+++ lib/tasks/phone_number_carrier_lookup.rb
@@ -21,14 +21,18 @@ class PhoneNumberCarrierLookup < BaseTask
   def run
     super
 
-    phone_number = _get_entity_attribute("name").gsub(".","").gsub(" ","").gsub("-","")
+    # Replace bad characters
+    #
+    # For future, consider:
+    # http://stackoverflow.com/questions/20650681/ruby-gsub-multiple-characters-in-string
+    phone_number = _get_entity_attribute("name").gsub(".","").gsub(" ","").gsub("-","").gsub("(","").gsub(")","")
+
+    # get the API key
     api_key = _get_global_config "carrierlookup_api_key"
 
-    balance_uri = "http://www.carrierlookup.com/index.php/api/balance?key=#{api_key}"
     lookup_uri = "http://www.carrierlookup.com/index.php/api/lookup?key=#{api_key}&number=#{phone_number}"
 
     begin
-      #@task_log.log "Current balance: #{http_get_body balance_uri}"
       response = http_get_body lookup_uri
       attributes = JSON.parse response if response
 
@@ -39,16 +43,15 @@ class PhoneNumberCarrierLookup < BaseTask
 
       @task_log.log "You have #{attributes["Response"]["creditBalance"]} credits remaining."
 
-      # edit the phone number entity
+      # Edit the phone number entity
       @entity.attributes["carrier_type"] = attributes["Response"]["carrier_type"]
       @entity.attributes["carrier"] = attributes["Response"]["carrier"]
-      @task_log.log "Updating PhoneNumber entity: #{@entity.inspect}"
+      @entity.save
+
       @task_log.log "Carrier Type: #{@entity.attributes["carrier_type"]}"
       @task_log.log "Carrier: #{@entity.attributes["carrier"]}"
 
-      @entity.save
-
-      # add an info entity_ids
+      # Add an info entity_ids
       #{}_create_entity "Info", attributes.merge({"name" => "Carrier Lookup for #{phone_number}: #{attributes}"})
 
     rescue JSON::ParserError
