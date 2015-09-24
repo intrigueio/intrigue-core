c37115c88a01641f090c6c5f3e382c5634e94d4b
jcran <jcran@pentestify.com>
2015-09-23 23:56:52 -0700

    add the ability to extract dns records in uri_spider, updates to default options

--- lib/tasks/uri_spider.rb
+++ lib/tasks/uri_spider.rb
@@ -1,4 +1,5 @@
 require 'arachnid'
+require 'uri'
 
 module Intrigue
 class UriSpider < BaseTask
@@ -18,9 +19,10 @@ class UriSpider < BaseTask
         {"type" => "Uri", "attributes" => { "name" => "http://www.intrigue.io" }}
       ],
       :allowed_options => [
-        {:name => "threads", :type => "Integer", :regex => "integer", :default => 3 },
-        {:name => "max_pages", :type => "Integer", :regex => "integer", :default => 250 },
-        {:name => "create_urls", :type => "Boolean", :regex => "boolean", :default => false },
+        {:name => "threads", :type => "Integer", :regex => "integer", :default => 1 },
+        {:name => "max_pages", :type => "Integer", :regex => "integer", :default => 1000 },
+        {:name => "extract_uris", :type => "Boolean", :regex => "boolean", :default => false },
+        {:name => "extract_dns_records", :type => "Boolean", :regex => "boolean", :default => false },
         {:name => "parse_metadata", :type => "Boolean", :regex => "boolean", :default => true }
         #{:name => "user_agent",  :type => "String",  :regex => "alpha_numeric", :default => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36"}
       ],
@@ -38,7 +40,8 @@ class UriSpider < BaseTask
     @opt_threads = _get_option("threads").to_i
     @opt_max_pages = _get_option("max_pages").to_i
     #@opt_user_agent = _get_option "user_agent"
-    @opt_create_urls = _get_option "create_urls" # create a Uri object for each page
+    @opt_extract_uris = _get_option "extract_uris" # create an object for each page
+    @opt_extract_dns_records = _get_option "extract_dns_records" # create an object for each dns_record
     @opt_parse_metadata = _get_option "parse_metadata" # create a Uri object for each page
 
     crawl_and_parse(uri)
@@ -48,61 +51,75 @@ class UriSpider < BaseTask
 
   def crawl_and_parse(uri)
     @task_log.log "Crawling: #{uri}"
+    dns_records = []
 
-    Arachnid.new(uri, {
-        :exclude_urls_with_images => true }).crawl({
-          :threads => @opt_threads,
-          :max_urls => @opt_max_pages}) do |response|
+    Arachnid.new(uri).crawl({
+        :threads => @opt_threads,
+        :max_urls => @opt_max_pages}) do |response|
 
-        begin
+      @task_log.log "Processing #{response.effective_url}"
 
-          # Extract the url
-          page_uri = "#{response.effective_url}".encode('UTF-8', {
-            :invalid => :replace,
-            :undef => :replace,
-            :replace => '?'})
+      # Extract the uri
+      page_uri = "#{response.effective_url}".encode('UTF-8', {
+        :invalid => :replace,
+        :undef => :replace,
+        :replace => '?'})
 
-          # Create an entity for this uri
-          _create_entity("Uri", { "name" => page_uri, "uri" => page_uri }) if @opt_create_urls
+      # Create an entity for this uri
+      _create_entity("Uri", { "name" => page_uri, "uri" => page_uri }) if @opt_extract_uris
 
-          # If we don't have a body, we can't do anything here.
-          next unless response.body
+      # If we don't have a body, we can't do anything here.
+      next unless response.body
 
-          # Extract the body
-          page_body = Nokogiri::HTML.parse(response.body.to_s.encode('UTF-8', {
-            :invalid => :replace,
-            :undef => :replace,
-            :replace => '?'})).to_s.encode('UTF-8', {
-              :invalid => :replace,
-              :undef => :replace,
-              :replace => '?'})
+      # Extract the body
+      page_body = Nokogiri::HTML.parse(response.body.to_s.encode('UTF-8', {
+        :invalid => :replace,
+        :undef => :replace,
+        :replace => '?'})).to_s.encode('UTF-8', {
+          :invalid => :replace,
+          :undef => :replace,
+          :replace => '?'})
 
-          if @opt_parse_metadata
-
-            # Get the filetype for this page
-            filetype = "#{page_uri.split(".").last.gsub("/","")}".upcase
-
-            # A list of all filetypes we're capable of doing something with
-            interesting_types = [
-              "BIN","DOC","DOCX","EPUB","EXE","ICA","INDD",
-              "MP3","MP4","ODG","ODP","ODS","ODT",
-              "PDF","PPS","PPSX","PPT","PPTX","PUB",
-              "RDP","SVG","SVGZ","SXC","SXI","SXW","TIF",
-              "TXT","WPD","XLS","XLSX"]
-
-            if interesting_types.include? filetype
-              download_and_extract_metadata page_uri
-            else
-              parse_entities_from_content(page_uri, page_body)
+      # Create an entity for this host
+      if @opt_extract_dns_records
+        @task_log.log "Extracting dns records from #{response.effective_uri}"
+        URI.extract(page_body, ["https","http"]) do |link|
+          begin
+            # Collect the host
+            host = URI(link).host
+            if host
+              _create_entity("DnsRecord", "name" => host, "uri" => page_uri) unless dns_records.include?(host)
+              dns_records << host
             end
-
-          else
-            @task_log.log "Parsing as a regular file"
-            parse_entities_from_content(page_uri, page_body)
+          rescue URI::InvalidURIError => e
+            @task_log.error "Error, unable to parse #{link} from page #{page_uri}"
           end
+        end
+      end
 
+      if @opt_parse_metadata
+
+        # Get the filetype for this page
+        filetype = "#{page_uri.split(".").last.gsub("/","")}".upcase
+
+        # A list of all filetypes we're capable of doing something with
+        interesting_types = [
+          "BIN","DOC","DOCX","EPUB","EXE","ICA","INDD",
+          "MP3","MP4","ODG","ODP","ODS","ODT",
+          "PDF","PPS","PPSX","PPT","PPTX","PUB",
+          "RDP","SVG","SVGZ","SXC","SXI","SXW","TIF",
+          "TXT","WPD","XLS","XLSX"]
+
+        if interesting_types.include? filetype
+          download_and_extract_metadata page_uri
+        else
+          parse_entities_from_content(page_uri, page_body)
         end
 
+      else
+        @task_log.log "Parsing as a regular file"
+        parse_entities_from_content(page_uri, page_body)
+      end
     end # end .crawl
   end # crawl_and_parse
 
