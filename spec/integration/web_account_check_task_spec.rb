d6ebb98d9581a6d9adc3fa3a7451bd1d70c4fb96
jcran <jcran@pentestify.com>
2015-10-05 09:42:15 -0700

    Update accounts. Note that some of these are still delivering false negatives. Update requesting user-agent before moving forward

--- spec/integration/web_account_check_task_spec.rb
+++ spec/integration/web_account_check_task_spec.rb
@@ -4,32 +4,33 @@ describe "Intrigue v1.0 Tasks" do
   describe "WebAccountCheckTask" do
 
 
-  it "checks each site in data/web_accounts_list.json for false negatives" do
+    it "checks each site in data/web_accounts_list.json for false negatives" do
 
-    @api = IntrigueApi.new
+      @api = IntrigueApi.new
 
-    account_list_data = File.open("data/web_accounts_list.json").read
-    account_list = JSON.parse(account_list_data)
+      account_list_data = File.open("data/web_accounts_list.json").read
+      account_list = JSON.parse(account_list_data)
 
-    account_list["sites"].each do |site|
-      puts "Site: #{site}"
+      account_list["sites"].each do |site|
+        puts "Checking site: #{site["name"]}"
 
-      # Create an entity that can be saved
-      entity = {
-        "type" => "String",
-        "attributes" => {
-          "name" => "#{site["known_accounts"].first}"
+        # Create an entity that can be saved
+        entity = {
+          "type" => "String",
+          "attributes" => {
+            "name" => "#{site["known_accounts"].first}"
+          }
         }
-      }
 
-      result = @api.start("web_account_check", entity, [{"name" => "specific_sites", "value" => "#{site["name"]}"}])
+        result = @api.start("web_account_check", entity, [{"name" => "specific_sites", "value" => "#{site["name"]}"}])
 
-      if result["entity_ids"].count < 1
-        puts "Found #{result["entity_ids"].count} accounts. #{site["check_uri"].gsub("{account}", site["known_accounts"].first)}"
+        if result["entity_ids"].count < 1
+          puts "Found #{result["entity_ids"].count} accounts. #{site["check_uri"].gsub("{account}", site["known_accounts"].first)}. FAIL."
+        end
+
+        # Fail if we don't get a result
+        expect(result["entity_ids"].count).to eq 1
       end
-
-      # Fail if we don't get a result
-      expect(result["entity_ids"].count).to eq 1
 
     end
 
@@ -50,6 +51,5 @@ describe "Intrigue v1.0 Tasks" do
       expect(result["entity_ids"].count).to be 0
     end
 
-    end
   end
 end
