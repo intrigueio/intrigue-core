593bfb75c29fbe4bcffc6386a6550829be751f2f
jcran <jcran@pentestify.com>
2015-11-11 17:46:35 -0800

    add a dns subdomain scan

---
+++ lib/scans/dns_subdomain_scan.rb
@@ -0,0 +1,27 @@
+module Intrigue
+module Scanner
+class DnsSubdomainScan < Intrigue::Scanner::Base
+
+    private
+
+    ### Main "workflow" function
+    #
+    def _recurse(entity, depth)
+
+      if depth <= 0      # Check for bottom of recursion
+        @scan_log.log "Returning, depth @ #{depth}"
+        return
+      end
+
+      if entity.type == "DnsRecord"
+        ### DNS Subdomain Bruteforce
+        _start_task_and_recurse "dns_brute_sub",entity,depth,[{"name" => "use_file", "value" => "true"}]
+      else
+        @scan_log.log "SKIP Unhandled entity type: #{entity.type}##{entity.attributes["name"]}"
+        return
+      end
+    end
+
+end
+end
+end
