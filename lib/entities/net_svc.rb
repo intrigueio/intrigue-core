5e0729d86c2894f43279e9360edb9b53a81cd430
jcran <jcran@pentestify.com>
2015-11-18 12:51:59 -0800

    fix a bug with netsvc entity

--- lib/entities/net_svc.rb
+++ lib/entities/net_svc.rb
@@ -17,9 +17,9 @@ class NetSvc < Intrigue::Model::Entity
 
   def form
     output = super
-    output << "IP Address: <input type=\"text\" name=\"attrib_ip_address\" value=#{ _escape_html @attributes[:ip_address]}><br/>"
-    output << "Port Num: <input type=\"text\" name=\"attrib_port_num\" value=#{ _escape_html @attributes[:port_num]}><br/>"
-    output << "Proto: <input type=\"text\" name=\"attrib_proto\" value=#{ _escape_html @attributes[:proto]}><br/>"
+    output << "IP Address: <input type=\"text\" name=\"attrib_ip_address\" value=#{ _escape_html @details["ip_address"]}><br/>"
+    output << "Port Num: <input type=\"text\" name=\"attrib_port_num\" value=#{ _escape_html @details["port_num"]}><br/>"
+    output << "Proto: <input type=\"text\" name=\"attrib_proto\" value=#{ _escape_html @details["proto"]}><br/>"
   output
   end
 
