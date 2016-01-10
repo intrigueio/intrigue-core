edd996fd1f0c99eed446f57da3900c013c9f0b8a
jcran <jcran@pentestify.com>
2016-01-10 15:09:19 -0800

    Only allow phone numbers for the phone_number_carrier_lookup task

--- lib/tasks/phone_number_carrier_lookup.rb
+++ lib/tasks/phone_number_carrier_lookup.rb
@@ -10,7 +10,7 @@ class PhoneNumberCarrierLookup < BaseTask
       :authors => ["jcran"],
       :description => "This task uses the CarrierLookup API to determine the provider of a phone number",
       :references => [],
-      :allowed_types => ["*"],
+      :allowed_types => ["PhoneNumber"],
       :example_entities => [ {"type" => "PhoneNumber", "attributes" => {"name" => "202-456-1414"}} ],
       :allowed_options => [ ],
       :created_types => [] # only edits the phone number
