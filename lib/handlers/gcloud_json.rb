1ae230d0a8d883da16ff65f786df769e15a67112
jcran <jcran@pentestify.com>
2016-03-08 01:27:30 -0800

    tweaks

--- lib/handlers/gcloud_json.rb
+++ lib/handlers/gcloud_json.rb
@@ -19,7 +19,7 @@ module Handler
       bucket_name = _get_handler_config("bucket_name")
       object_name = "#{result.task_name}_on_#{result.base_entity.name}.json"
 
-      gcloud = Gcloud.new 
+      gcloud = Gcloud.new
       storage = gcloud.storage
       bucket = storage.bucket bucket_name
 
@@ -27,7 +27,7 @@ module Handler
       temp_file = Tempfile.new("gcloud_json")
       temp_file.write JSON.pretty_generate(result.export_hash)
 
-      bucket.create_file temp_file.path object_name
+      bucket.create_file temp_file.path, object_name
 
       temp_file.close
       temp_file.unlink
