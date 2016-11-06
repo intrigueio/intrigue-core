cea6447971331249801f3ea2539f26c425092585
jcran <jcran@pentestify.com>
2016-11-06 07:18:13 -0800

    Pull out the page id ... DRY

--- app/routes/entities.rb
+++ app/routes/entities.rb
@@ -5,18 +5,19 @@ class IntrigueApp < Sinatra::Base
       @project_name = params[:project]
       @entity_type = params[:entity_type]
       @entity_name = params[:entity_name]
+      @page_id = params[:page]
       ## We have some very rudimentary searching capabilities here
       ##
       ## TODO - these should be fleshed out so we can actually use the entities page
       ##
       scoped_entities = Intrigue::Model::Entity.scope_by_project(@project_name)
-      if
+      if @entity_type
         entity_type_string = eval("Intrigue::Entity::#{@entity_type}")
-        @entities = scoped_entities.all(:type.like => entity_type_string).page(params[:page], :per_page => 50)
+        @entities = scoped_entities.all(:type.like => entity_type_string).page(@page_id, :per_page => 50)
       elsif @entity_name
-        @entities = scoped_entities.all(:name.like => "%#{@entity_name}%").page(params[:page], :per_page => 50)
+        @entities = scoped_entities.all(:name.like => "%#{@entity_name}%").page(@page_id, :per_page => 50)
       else
-        @entities = scoped_entities.page(params[:page], :per_page => 50)
+        @entities = scoped_entities.page(@page_id, :per_page => 50)
       end
 
       erb :'entities/index'
@@ -24,7 +25,8 @@ class IntrigueApp < Sinatra::Base
 
     get '/:project/entities' do
       @project_name = params[:project]
-      @entities = Intrigue::Model::Entity.scope_by_project(@project_name).page(params[:page], :per_page => 50)
+      @page_id = params[:page]
+      @entities = Intrigue::Model::Entity.scope_by_project(@project_name).page((@page_id, :per_page => 50)
       erb :'entities/index'
     end
 
