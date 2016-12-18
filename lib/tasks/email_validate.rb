d38f58a55701536c36a314b5e37001360b08ba78
jcran <jcran@pentestify.com>
2016-12-17 17:19:30 -0800

    clean up some old / unused tasks

--- lib/tasks/email_validate.rb
+++ lib/tasks/email_validate.rb
@@ -1,14 +1,14 @@
 module Intrigue
-class EmailValidateTask < BaseTask
+class EmailValidateMailboxLayerTask < BaseTask
   include Intrigue::Task::Web
 
 
   def self.metadata
     {
-      :name => "email_validate",
-      :pretty_name => "Email Validate",
+      :name => "email_validate_mailbox_layer",
+      :pretty_name => "Email Validate via MailboxLayer",
       :authors => ["jcran"],
-      :description => "This task validates an email via the email-validator.net API.",
+      :description => "This task validates an email via the MailboxLayer API.",
       :requires_config => [""],
       :references => [],
       :type => "discovery",
@@ -27,21 +27,38 @@ class EmailValidateTask < BaseTask
     super
 
     email_address = _get_entity_name
-    api_key = _get_global_config "email_validator_apikey"
-    uri = "http://api.email-validator.net/api/verify?EmailAddress=#{email_address}&APIKey=#{api_key}"
+    api_key = _get_global_config "mailbox_layer_apikey"
+    uri = "https://apilayer.net/api/check?access_key=#{api_key}&email=#{email_address}&smtp=1&format=1"
 
     email_validation_results = JSON.parse http_get_body(uri)
-    _log "Got result: #{email_validation_results["status"]}"
+    _log "Got result: #{email_validation_results}"
 
-    if email_validation_results["info"] =~ /Valid Address/
+=begin
+{
+  "email":"test@intrigue.io",
+  "did_you_mean":"",
+  "user":"test",
+  "domain":"intrigue.io",
+  "format_valid":true,
+  "mx_found":true,
+  "smtp_check":true,
+  "catch_all":null,
+  "role":false,
+  "disposable":false,
+  "free":false,
+  "score":0.96
+}
+=end
+
+    if email_validation_results["smtp_check"]
+      _log_good "Got a valid address"
       #_create_entity "Info", email_validation_results.merge({"name" => "Valid Address: #{email_address}"})
-      @entity.details["verified"] = true
-      @entity.details.merge email_validation_results
     else
-      @entity.details["verified"] = false
-      @entity.details.merge email_validation_results
+      _log_error "Got an invalid address"
     end
-  end
 
+    @entity.details = @entity.details.merge(email_validation_results)
+
+  end
 end
 end
