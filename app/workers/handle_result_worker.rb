b67166bafaf391d76d4b99459d32cd6e247a8857
jcran <jcran@pentestify.com>
2017-02-02 19:35:42 -0800

    no need to run if we're already complete

--- app/workers/handle_result_worker.rb
+++ app/workers/handle_result_worker.rb
@@ -13,9 +13,16 @@ class HandleResultWorker
   def perform(id)
     puts "HandleResultWorker: Starting background worker task for handlers... "
 
+
+    # if there's not going to be any new information, let's just return
+    if scan_result.handlers_complete
+      puts "returning, handlers have already been marked complete"
+      return
+    end
+
     # Get the scan_result & the incomplete tasks
     scan_result = Intrigue::Model::ScanResult.first(:id => id)
-=begin
+
     incomplete_task_count = scan_result.task_results.select{|tr| tr.complete == false }.count
 
     # Make sure we actually have handlers before starting
@@ -24,6 +31,7 @@ class HandleResultWorker
       return
     end
 
+=begin
     until incomplete_task_count == 0
       puts "HandleResultWorker: Waiting for #{scan_result.name} to finish... #{incomplete_task_count}/#{scan_result.task_results.count} tasks."
       sleep 10
@@ -36,18 +44,22 @@ class HandleResultWorker
     # If we don't have any, mark the scan_result complete
     puts "HandleResultWorker: All complete, handling... #{scan_result.name}"
 =end
+
     # and then run the handlers
     scan_result.handlers.each do |handler_type|
       handler = Intrigue::HandlerFactory.create_by_type(handler_type)
       puts "HandleResultWorker: Calling #{handler_type} handler on #{scan_result.name}"
       handler.process(scan_result)
     end
-=begin
-    # ...and mark the handlers run
-    scan_result.handlers_complete = true
-    scan_result.complete = true
-    scan_result.save
-=end
+
+    # let's mark it complete if there's nothing else to do here.
+    if incomplete_task_count == 0
+      # ...and mark the handlers run
+      scan_result.handlers_complete = true
+      scan_result.complete = true
+      scan_result.save
+    end
+
   end
 
 end
