9027257dd6866e213db0939ba103d9d3df87583d
jcran <jcran@pentestify.com>
2017-02-14 21:46:19 -0800

    don't time out handlers

--- app/workers/handle_result_worker.rb
+++ app/workers/handle_result_worker.rb
@@ -25,27 +25,27 @@ class HandleResultWorker
     end
 
 
-    ### So... here we wait for the result's tasks to complete... unless we hit our
-    ### countdown timer, or the user has specifically requested that we don't (This
+    ### So... here we wait for the result's tasks to complete... unless we complete
+    ### our tasks, or the user has specifically requested that we don't (This
     ### is handy in cases of requesting to handle the results via CLI)
 
-    # failsafe ... never let us wait more than sleep_length * i
+    # default sleep length
     sleep_length = 60
-    i = 20
+    i = 0
 
     # Get our current number of incomplete tasks
     incomplete_task_count = scan_result.task_results.select{|tr| tr.complete == false }.count
 
     # and then check to make sure we're not done, or timed out, or forced
-    until (incomplete_task_count == 0 || i < 0 || force_handling==true)
+    until (incomplete_task_count == 0 || force_handling==true)
       puts "HandleResultWorker #{id}: Waiting for #{scan_result.name} to finish... #{incomplete_task_count}/#{scan_result.task_results.count} tasks."
-      puts "HandleResultWorker #{id}: Countdown... #{i}"
+      puts "HandleResultWorker #{id}: Countdown... #{i*sleep_length} seconds"
       sleep sleep_length
 
       # Grab the record again, so we can make sure to have the latest info
       scan_result = Intrigue::Model::ScanResult.first(:id => id)
       incomplete_task_count = scan_result.task_results.select{|tr| tr.complete == false }.count
-      i=i-1
+      i=i+1
     end
 
     # and then run the handlers
