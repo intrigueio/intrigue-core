04ffed81fbdd928927677fb312a30513c009d474
jcran <jcran@pentestify.com>
2017-08-12 18:32:52 -0700

    handy aliasing...

--- util/core-console.rb
+++ util/core-console.rb
@@ -3,6 +3,11 @@
 require_relative '../core'
 require 'pry'
 
+alias _log puts
+alias _log_good puts
+alias _log_error puts
+alias _log_debug puts
+
 ###
 ### Define the prompt & drop into pry repl
 ###
