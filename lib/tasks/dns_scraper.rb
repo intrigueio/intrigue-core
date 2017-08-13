37a19d54c42f53a5aee8472d7849288e82fb046b
jcran <jcran@pentestify.com>
2017-08-12 18:32:32 -0700

    virustotal scraping

--- lib/tasks/dns_scraper.rb
+++ lib/tasks/dns_scraper.rb
@@ -25,13 +25,30 @@ class DnsScraper < BaseTask
     search_domain = _get_entity_name
     search_uri = "https://api.sublist3r.com/search.php?domain=#{search_domain}"
     begin
-      domains = JSON.parse(http_get_body(search_uri))
-    rescue JSON::ParserError
-      _log_error "Unable to get parsable response from #{search_uri}"
+      sublister_domains = JSON.parse(http_get_body(search_uri))
+      _log_good "Got sublister domains: #{sublister_domains}"
+      sublister_domains.each{|d| _create_entity("DnsRecord", {"name" => "#{d}"})}
+    rescue JSON::ParserError => e
+      _log_error "Unable to get parsable response from #{search_uri}: #{e}"
+    rescue StandardError => e
+      _log_error "Error grabbing sublister domains: #{e}"
     end
-    domains.each{|d| _create_entity("DnsRecord", {"name" => "#{d}"})}
 
 
+    # Virustotal API (this'll be blocked after a couple queries... better to use the api)
+    # API Docs: https://www.virustotal.com/en/documentation/public-api/#getting-domain-reports
+    virustotal_uri = "https://www.virustotal.com/en/domain/#{search_domain}/information/"
+    begin
+      raw_html = http_get_body virustotal_uri
+      html = Nokogiri::HTML(raw_html)
+      vt_domains = html.xpath("//*[@id='observed-subdomains']/div/a/text()").map do |x|
+        x.to_s.gsub("\n","").strip
+      end
+      _log_good "Got virustotal domains: #{vt_domains}"
+      vt_domains.each{|d| _create_entity "DnsRecord", {"name" => "#{d}"} }
+    rescue StandardError => e
+      _log_error "Error grabbing virustotal domains: #{e}"
+    end
 
   end
 
