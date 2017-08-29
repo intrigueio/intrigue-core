cda17ce5e85def1dabda6e869b91ad825f98cd34
jcran <jcran@pentestify.com>
2017-08-29 08:47:47 -0700

    clean up search / csv export functions

--- app/routes/entities.rb
+++ app/routes/entities.rb
@@ -30,7 +30,7 @@ class IntrigueApp < Sinatra::Base
         selected_entities = selected_entities.where(:type => @entity_types) if @entity_types
 
         # Perform a simple tokenized search
-        selected_entities = _tokenized_search(@search_string, selected_entities)
+        selected_entities = _tokenized_search(@search_string, selected_entities) if @search_string
 
         ## paginate
         @entity_count = selected_entities.count
@@ -44,17 +44,18 @@ class IntrigueApp < Sinatra::Base
     content_type 'text/csv'
 
     params[:search_string] == "" ? @search_string = nil : @search_string = params[:search_string]
-    params[:entity_types] == "" ? @entity_types = nil : @entity_types = params[:entity_types]
+    params[:entity_types] == [""] ? @entity_types = nil : @entity_types = params[:entity_types]
     params[:correlate] == "on" ? @correlate = true : @correlate = false
     (params[:page] != "" && params[:page].to_i > 0) ? @page = params[:page].to_i : @page = 1
 
-    selected_entities = Intrigue::Model::Entity.scope_by_project(@project_name).where(:hidden=>false).order(:name)
+    selected_entities = Intrigue::Model::Entity.scope_by_project(@project_name).order(:name)
 
     ## Filter if we have a type
     selected_entities = selected_entities.where(:type => @entity_types) if @entity_types
 
     # Perform a simple tokenized search
-    selected_entities = _tokenized_search(@search_string, selected_entities)
+
+    selected_entities = _tokenized_search(@search_string, selected_entities) if @search_string
 
     out = ""
     out << "Type,Name,Aliases,Details\n"
