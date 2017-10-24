2bf4a18f0391e8285a3c8a9578ebb3595bf103dc
jcran <jcran@pentestify.com>
2017-10-23 22:41:57 -0700

    skip a few more common  services

--- lib/tasks/helpers/data.rb
+++ lib/tasks/helpers/data.rb
@@ -70,6 +70,7 @@ module Data
         entity_name =~ /^.*cheetahmail.com(:[0-9]*)?$/               ||
         entity_name =~ /^.*cloudapp.net(:[0-9]*)?$/                  ||
         entity_name =~ /^.*cloudfront.net(:[0-9]*)?$/                ||
+        entity_name =~ /^.*decipherinc.com(:[0-9]*)?$/               ||
         entity_name =~ /^.*discourse.org(:[0-9]*)?$/                 ||
         entity_name =~ /^.*drupal.org(:[0-9]*)?$/                    ||
         entity_name =~ /^.*edgecastcdn.net(:[0-9]*)?$/               ||
@@ -126,6 +127,7 @@ module Data
         entity_name =~ /^.*metric.gstatic.com(:[0-9]*)?$/            ||
         entity_name =~ /^.*mktoweb.com(:[0-9]*)?$/                   ||
         entity_name =~ /^.*microsoft.com(:[0-9]*)?$/                 ||
+        entity_name =~ /^.*mtsvc.net(:[0-9]*)?$/                     ||
         entity_name =~ /^.*msn.com(:[0-9]*)?$/                       ||
         entity_name =~ /^.*oclc.org(:[0-9]*)?$/                      ||
         entity_name =~ /^.*office365.com(:[0-9]*)?$/                 ||
