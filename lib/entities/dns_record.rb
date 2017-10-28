e3bd7e3ff1e0817b78b3b972035ad6b498fd1f81
jcran <jcran@pentestify.com>
2017-10-28 09:13:13 -0700

    fix detail string

--- lib/entities/dns_record.rb
+++ lib/entities/dns_record.rb
@@ -19,9 +19,9 @@ class DnsRecord < Intrigue::Model::Entity
 
   def detail_string
     if details["lookup_data"]
-      details["lookup_data"].map{|x| x["name"] }.sort.uniq.join(", ")
+      details["lookup_data"].map{|x| x["name"] if x }.sort.uniq.join(", ")
     else
-      nil
+      ""
     end
   end
 
