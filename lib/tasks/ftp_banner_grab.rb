76a48d8620cbdc236567d63371d0fa206d754fe0
jcran <jcran@pentestify.com>
2018-03-27 12:18:28 -0700

    fix up ftp enumeration, add a more sophisticated task

--- lib/tasks/ftp_banner_grab.rb
+++ lib/tasks/ftp_banner_grab.rb
@@ -35,44 +35,41 @@ class FtpBannerGrab < BaseTask
     super
 
     port = _get_entity_attribute("port").to_i
-    protocol = _get_entity_attribute "proto"
+    protocol = _get_entity_attribute "protocol"
     ip_address = _get_entity_attribute "ip_address"
 
-    #_log "Port: #{port}"
-    #_log "Protocol: #{protocol}"
-    #_log "IP Address: #{ip_address}"
-
     # Check to make sure we have a sane target
-    if protocol.downcase == "tcp"
-      if ip_address and port
-        s = TCPSocket.new(ip_address, port)
-      else
-        raise ArgumentError, "Missing IP Address and Port. Unable to open a socket."
+    if protocol.downcase == "tcp" && ip_address && port
+
+      banner = ""
+
+      sockets = Array.new #select() requires an array
+      #fill the first index with a socket
+      sockets[0] = TCPSocket.open(ip_address, port)
+      while true #loop till it breaks
+
+      #listen for a read, timeout 3
+      res = select(sockets, nil, nil,3)
+        if res != nil  # a nil is a timeout and will break
+              #THIS PRINTS NIL FOREVER on a server crash
+          banner << sockets[0].gets()
+        else
+          sockets[0].close
+          break
+        end
       end
+
+
+      if banner.length > 0
+        _log "Got banner for #{ip_address}:#{port}/#{protocol}: #{banner}"
+        _log "updating entity with banner info!"
+        @entity.set_detail "banner", banner
+      else
+        _log "No banner available"
+      end
+
     end
 
-    unless s
-      _log_error "Unable to open socket, quitting!"
-      return
-    end
-
-    # Probe the port
-    begin
-      banner = s.gets(10000)
-
-      _log "Got banner for #{ip_address}:#{port}/#{protocol}: #{banner}"
-      _log "updating entity with banner info!"
-
-      @entity.set_detail "banner", banner
-
-    rescue Errno::EPIPE
-      _log "Broken Pipe"
-    rescue Errno::ECONNRESET
-      _log "Connection Reset"
-    end
-
-    # Cleanup
-    s.close
   end
 
 end
---
+++ lib/tasks/ftp_enumerate.rb
@@ -0,0 +1,102 @@
+require 'net/ftp'
+
+###
+### XXX - This module has not been extensively tested!
+###
+module Intrigue
+module Task
+class FtpEnumerate < BaseTask
+
+  def self.metadata
+    {
+      :name => "ftp_enumerate",
+      :pretty_name => "Enumerate an FTP server",
+      :authors => ["jcran"],
+      :description => "This task connects to an FTP service and collects information.",
+      :references => [],
+      :type => "discovery",
+      :passive => false,
+      :allowed_types => ["FtpService"],
+      :example_entities => [
+        {"type" => "FtpService", "details" => {
+          "ip_address" => "1.1.1.1",
+          "port" => 1111,
+          "protocol" => "tcp"
+          }
+        }
+      ],
+      :allowed_options => [
+        #{:name => "port_num", :type => "Integer", :regex => "integer", :default => 111 }
+      ],
+      :created_types => []
+    }
+  end
+
+  ## Default method, subclasses must override this
+  def run
+    super
+
+    port = _get_entity_attribute("port").to_i
+    protocol = _get_entity_attribute "protocol"
+    ip_address = _get_entity_attribute "ip_address"
+
+    # Check to make sure we have a sane target
+    if protocol.downcase == "tcp" && ip_address && port
+
+      begin
+
+        ftp = Net::FTP.open("#{ip_address}") do |ftp|
+
+          ftp.passive = true
+
+          out = {}
+          begin
+            out["anonymous"] = ftp.login
+          rescue Net::FTPPermError => e
+            _log_error "unable to log in, proceeding"
+          end
+
+          begin
+              out["help"] = "#{ftp.help}"
+          rescue Net::FTPPermError => e
+            _log_error "unable to run HELP, proceeding"
+          end
+
+          begin
+            ftp.chdir('/')
+            out["dir"] = [
+              "root" => {
+                "listing" => ftp.list,
+                "facts" => ftp.mlst.facts
+              }
+            ]
+          rescue Net::FTPPermError => e
+            _log_error "unable to collect directory info, not logged in"
+          end
+
+          begin
+            out["system"] = "#{ftp.system}"
+          rescue Net::FTPPermError => e
+            _log_error "unable to collect system info - not logged in"
+          end
+
+          @entity.set_detail("ftp_enumerate", out)
+          _log out
+
+        end
+
+      rescue Net::FTPPermError=> e
+        _log_error "Invalid creds: #{e}"
+      rescue Errno::ECONNREFUSED => e
+        _log_error "Unable to connect: #{e}"
+      rescue Net::ReadTimeout => e
+        _log_error "Timeout: #{e}"
+      end
+
+    end
+
+  end
+
+end
+end
+end
