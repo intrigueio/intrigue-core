5d67e285537f975397a707955acf4462d8729c0d
jcran <jcran@pentestify.com>
2018-04-10 23:55:04 -0700

    security trails api placeholder

--- lib/tasks/search_security_trails.rb
+++ lib/tasks/search_security_trails.rb
@@ -28,68 +28,7 @@ class SearchSecurityTrails < BaseTask
       api_key = _get_global_config "security_trails_api_key"
       entity_name = _get_entity_name
 
-      case _get_entity_type_string
-
-        when "EmailAddress"
-          entity_type = "email"
-
-        when "DnsRecord"
-
-          ## When we have a host, we need to do a lookup on the current record,
-          ## grab the email address, and then do the search based on that email
-
-          _log "Looking up contacts for domain"
-          begin
-            # We're going to pull the domain's email address....
-            whois = ::Whois::Client.new(:timeout => 20)
-            answer = whois.lookup(entity_name)
-            # Run through the contacts and pick the first one
-            contact_emails = answer.parser.contacts.map{ |contact| contact.email }
-            _log "Got contact_emails: #{contact_emails}"
-          rescue Timeout::Error => e
-            _log_error "Unable to lookup #{entity_name}... try a manual lookup"
-            return nil
-          end
-
-          entity_name = contact_emails.first
-          entity_type = "email"
-      end
-
-      _log "Got entity: #{entity_type} #{entity_name}"
-
-      unless entity_name
-        # Something went wrong with the lookup?
-        _log "Unable to get a current email address"
-        return
-      end
-
-      unless api_key
-        _log_error "No api_key?"
-        return
-      end
-
-      # Attach to the whoisology service & search
-      whoisology = Whoisology::Api.new(api_key)
-
-      # Run a PING to see if we have any results
-      result = whoisology.ping entity_type, entity_name
-      _log "Got #{result}"
-      _log "Got #{result["count"]} results"
-      return if result["count"].to_i == 0
-
-      # do the actual search with the FLAT command
-      result = whoisology.flat entity_type, entity_name
-
-      _log_good "Creating entities for #{result["count"]} results."
-      if result["domains"]
-        result["domains"].each {|d| _create_entity "DnsRecord", {"name" => d["domain_name"]} }
-      else
-        _log_error "No domains, do we have API credits?"
-      end
-
-    rescue RuntimeError => e
-      _log_error "Runtime error: #{e.inspect}"
-    end
+      raise "Not yet implemented"
 
   end # end run()
 
