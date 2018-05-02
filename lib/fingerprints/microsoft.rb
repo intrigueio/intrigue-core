c7522a253dd4e570791c4066a57e6227f4be71be
jcran <jcran@pentestify.com>
2018-05-02 02:13:37 -0500

    add fingerprint for microsoft forefront

---
+++ lib/fingerprints/microsoft.rb
@@ -0,0 +1,22 @@
+module Intrigue
+  module Fingerprint
+    class Microsoft < Intrigue::Fingerprint::Base
+
+      def generate_fingerprints(uri)
+        {
+          :uri => "#{uri}",
+          :checklist => [
+            {
+              :name => "Microsoft Forefront TMG",
+              :description => "Microsoft Forefront Threat Management Gateway",
+              :version => "Unknown",
+              :type => :content_cookies,
+              :content => /<title>Microsoft Forefront TMG/
+            }
+          ]
+        }
+      end
+
+    end
+  end
+end
