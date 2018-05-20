741e1d4e04bac13f7d796f0840e5b5a7f119ac10
jcran <jcran@pentestify.com>
2018-05-19 22:00:30 -0700

    catch error

--- lib/tasks/helpers/web.rb
+++ lib/tasks/helpers/web.rb
@@ -320,6 +320,8 @@ module Task
         @task_result.logger.log_error "Timeout : #{e}" if @task_result
       rescue Errno::ETIMEDOUT => e
         @task_result.logger.log_error "Timeout : #{e}" if @task_result
+      rescue Errno::EINVAL => e
+        @task_result.logger.log_error "Unable to connect: #{e}" if @task_result
       rescue Errno::ENETUNREACH => e
         @task_result.logger.log_error "Unable to connect: #{e}" if @task_result
       rescue Errno::EHOSTUNREACH => e
