707c259624fc52551a9d60cb082e3d14a04e1c3c
jcran <jcran@pentestify.com>
2018-07-06 01:50:01 -0500

    bump version

--- lib/ident/lib/intrigue-ident.rb
+++ lib/ident/lib/intrigue-ident.rb
@@ -11,7 +11,7 @@ Dir["#{check_folder}/*.rb"].each { |file| require_relative file }
 module Intrigue
   module Ident
 
-    VERSION=0.43
+    VERSION=0.44
 
     def generate_requests_and_check(url)
 
