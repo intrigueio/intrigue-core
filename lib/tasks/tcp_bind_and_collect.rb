7da18329578173b69862fd20798b35811214a0a0
jcran <jcran@pentestify.com>
2018-08-30 01:03:38 -0500

    add a task to listen for connections

---
+++ lib/tasks/tcp_bind_and_collect.rb
@@ -0,0 +1,74 @@
+require 'thread'
+require 'socket'
+
+module Intrigue
+module Task
+class TcpBindAndCollect < BaseTask
+
+  def self.metadata
+    {
+      :name => "tcp_bind_and_collect",
+      :pretty_name => "TCP Bind And Collect",
+      :authors => ["jcran"],
+      :description => "Given a set of ports (or all), bind and collect all connections",
+      :references => [],
+      :type => "discovery",
+      :passive => false,
+      :allowed_types => ["String"],
+      :example_entities =>  [{"type" => "String", "details" => {"name" => "default"}}],
+      :allowed_options => [
+        {:name => "ports", :regex=> "alpha_numeric", :default => "5000,8000,8080,8081,8443" }
+      ],
+      :created_types => ["String"]
+    }
+  end
+
+  def run
+    super
+    bind_and_listen _get_option("ports").split(",")
+  end
+
+  def track_connection(c)
+    _log "#{c}"
+  end
+
+  def bind_and_listen(ports=[])
+
+    ports = (0..65535) if ports.empty?
+
+    # Create threads to listen to each port
+    threads = ports.map do |port|
+      Thread.new do
+        begin
+          server = TCPServer.new port
+          while true do
+            c = server.accept    # Wait for a client to connect
+            connection = {}
+            connection["timestamp"] = DateTime.now
+            connection["listening_address"] = "#{c.addr}"
+            connection["source_address"] = "#{c.peeraddr}"
+            connection["message"] = ""
+            c.each_line do |line|
+              connection["message"] << line
+            end
+            c.close
+            track_connection connection
+          end
+        rescue Errno::EADDRINUSE => e
+          _log_error "Unable to bind, #{port} in use: #{e}"
+        rescue Errno::EMFILE => e
+          _log_error "too many files, or bind failed: #{e}"
+        rescue Errno::EACCES => e
+          _log_error "unable to bind: #{e}"
+        end
+      end
+    end
+
+    # Wait for all threads to complete
+    threads.map &:join
+  end
+
+
+end
+end
+end
