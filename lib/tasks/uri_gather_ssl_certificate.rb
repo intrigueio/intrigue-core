3bc057b2c729dff0160a707faf47540e9bb67f87
jcran <jcran@pentestify.com>
2018-09-11 19:48:53 -0500

    remove unused method

--- lib/tasks/uri_gather_ssl_certificate.rb
+++ lib/tasks/uri_gather_ssl_certificate.rb
@@ -210,22 +210,6 @@ class UriGatherSslCert  < BaseTask
     end
   end
 
-
-  def create_socket_with_timeout(host, port, timeout=nil)
-    addr = Socket.getaddrinfo(host, nil)
-    sock = Socket.new(Socket.const_get(addr[0][0]), Socket::SOCK_STREAM, 0)
-
-    if timeout
-      secs = Integer(timeout)
-      usecs = Integer((timeout - secs) * 1_000_000)
-      optval = [secs, usecs].pack("l_2")
-      sock.setsockopt Socket::SOL_SOCKET, Socket::SO_RCVTIMEO, optval
-      sock.setsockopt Socket::SOL_SOCKET, Socket::SO_SNDTIMEO, optval
-    end
-    #sock.connect(Socket.pack_sockaddr_in(port, addr[0][3]))
-  sock
-  end
-
 end
 end
 end
