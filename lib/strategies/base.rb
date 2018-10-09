9676d2f9cd4e599b9dbfb5098bcef62445480a0e
jcran <jcran@pentestify.com>
2018-10-08 23:13:13 -0500

    always wait for enrichment

--- lib/strategies/base.rb
+++ lib/strategies/base.rb
@@ -28,21 +28,12 @@ module Strategy
       project = old_task_result.project
 
       # hold on recursion until we're enriched
-      max_wait_iterations = 60
       until (entity.enriched || entity.enrichment_tasks.empty?)
         # make sure we re-lookup so we don't get stuck in loop
         entity = Intrigue::Model::Entity.first :id => entity.id
 
-        # ... enrichment should be fast
-        # don't get stuck in a loop forever (3 mins max)
-        max_wait_iterations-=1
-        if max_wait_iterations < 0
-          #puts "Max enrichment wait exceeded for: #{entity.type} #{entity.name}"
-          break
-        end
-
-        sleep 3
-        #puts "Waiting on enrichment... #{entity.type} #{entity.name}: #{entity.enriched}"
+        sleep 5
+        old_task_result.log "Waiting on enrichment... #{entity.type} #{entity.name}: #{entity.enriched}"
       end
 
       # check to see if it already exists, return nil if it does
