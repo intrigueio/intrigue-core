75a18903f67d987c60e0eb65efccedecc8c4fa94
jcran <jcran@pentestify.com>
2018-10-22 09:22:49 -0500

    remove snmp enrichment and move it to a task

--- lib/tasks/enrich/snmp_service.rb
+++ lib/tasks/enrich/snmp_service.rb
@@ -8,8 +8,8 @@ class SnmpService < Intrigue::Task::BaseTask
       :name => "enrich/snmp_service",
       :pretty_name => "Enrich SnmpService",
       :authors => ["jcran"],
-      :description => "Fills in details for an SNMP service",
-      :references => ["https://community.rapid7.com/community/services/blog/2016/05/05/snmp-data-harvesting-during-penetration-testing"],
+      :description => "Enrich an SNMP service",
+      :references => [],
       :type => "enrichment",
       :passive => false,
       :allowed_types => ["SnmpService"],
@@ -29,41 +29,7 @@ class SnmpService < Intrigue::Task::BaseTask
 
   ## Default method, subclasses must override this
   def run
-
-    port = _get_entity_detail("port").to_i || 161
-    protocol = _get_entity_detail "protocol" || "udp"
-    ip_address = _get_entity_detail "ip_address"
-
-    _log "Port: #{port}"
-    _log "Protocol: #{protocol}"
-    _log "IP Address: #{ip_address}"
-
-    # Check to make sure we have a sane target
-    if ip_address && port
-
-      # SNMPwalk
-      begin
-        output = ""
-        SNMP::Manager.open(:host => ip_address, :port => port) do |manager|
-          ifTable = SNMP::ObjectId.new("1.")
-          next_oid = ifTable
-          while next_oid.subtree_of?(ifTable)
-              response = manager.get_next(next_oid)
-              varbind = response.varbind_list.first
-              next_oid = varbind.name
-              _log "#{varbind}"
-              output << varbind.to_s
-          end
-        end
-        _set_entity_detail("snmp_output", _encode_string(output))
-      rescue SNMP::RequestTimeout => e
-        _log_error "SNMP Timeout"
-      end
-
-    else
-      raise ArgumentError, "Missing IP Address and Port. Unable to open a socket."
-    end
-
+    _log "Enriching... SNMP service: #{_get_entity_name}"
   end
 
 end
