1d8de8e82ee3b2a7c32cebc37af21d57e2cbd4fc
jcran <jcran@pentestify.com>
2018-10-27 09:45:05 -0500

    cleanup to help with memory consumption

--- lib/ident/lib/cpe.rb
+++ lib/ident/lib/cpe.rb
@@ -40,42 +40,45 @@ class Cpe
         #"#{nvd_data_directory}/nvdcve-1.0-2011.json"
       ]
 
-    files.each do |f|
+    files.each do |file|
       #puts "DEBUG Checking file: #{f}"
-      next unless File.exist? f
+      next unless File.exist? file
 
       # open and read the file
-      json = Yajl::Parser.new.parse(File.open(f,"r"))
+      f = File.open(file,"r")
+      parser = Yajl::Parser.new
+      json = parser.parse(f)
+
+      # free memory
+      parser = nil
+      f.close
 
       json["CVE_Items"].each do |v|
 
         # Note that the JSON has CVE stuff under a hash, so
         # pull that out separately (probably due to NVD responsibility vs Mitre)
-        cve = v["cve"]
 
         # data sanity check
-        unless cve["affects"] && cve["affects"]["vendor"] && cve["affects"]["vendor"]["vendor_data"]
-          #puts "DEBUG No Affects data... #{cve["cve"]}"
+        unless v["cve"]["affects"] && v["cve"]["affects"]["vendor"] && v["cve"]["affects"]["vendor"]["vendor_data"]
+          #puts "DEBUG No Affects data... #{v["cve"]}"
           next
         end
 
         # check to make sure it includes our vendor
-        vendors = cve["affects"]["vendor"]["vendor_data"].map{|x| x["vendor_name"].downcase }
-        unless vendors.uniq.include? @vendor.downcase
-          #vd = cve["affects"]["vendor"]["vendor_data"]
-          #puts "DEBUG No vendor match: #{@vendor.downcase} in #{vendors.uniq.count} vendors..."
-          next
-        end
+        vendors = v["cve"]["affects"]["vendor"]["vendor_data"].map{|x| x["vendor_name"].downcase }
+        next unless vendors.uniq.include? @vendor.downcase
 
         # iterate through the heirarchy to get to product and version we can match on
-        cve["affects"]["vendor"]["vendor_data"].each do |vd|
-          next if matched.include? cve["CVE_data_meta"]["ID"] # skip if we got it already
+        v["cve"]["affects"]["vendor"]["vendor_data"].each do |vd|
+          next if matched.include? v["cve"]["CVE_data_meta"]["ID"] # skip if we got it already
+
           #puts "DEBUG Checking Vendor Data #{vd}"
           vd["product"]["product_data"].each do |p|
-            next if matched.include? cve["CVE_data_meta"]["ID"] # skip if we got it already
+            next if matched.include? v["cve"]["CVE_data_meta"]["ID"] # skip if we got it already
+
             #puts "DEBUG Checking product #{p}"
             p["version"]["version_data"].each do |vd|
-              next if matched.include? cve["CVE_data_meta"]["ID"] # skip if we got it already
+              next if matched.include? v["cve"]["CVE_data_meta"]["ID"] # skip if we got it already
               next unless p["product_name"].downcase == @product.downcase
               #puts "DEBUG Matching: #{vd["version_value"]} with #{@version}"
 
@@ -87,14 +90,14 @@ class Cpe
               our_version = Versionomy.parse(@version)
 
               if vuln_version >= our_version
-                #puts "DEBUG - VULN MATCHED (#{cve['CVE_data_meta']['ID']}): #{Versionomy.parse(vd['version_value'])} >= #{Versionomy.parse(@version)}}!"
-                matched << cve["CVE_data_meta"]["ID"]
-                cve_id = cve["CVE_data_meta"]["ID"]
+                #puts "DEBUG - VULN MATCHED (#{v["cve"]['CVE_data_meta']['ID']}): #{Versionomy.parse(vd['version_value'])} >= #{Versionomy.parse(@version)}}!"
+                matched << v["cve"]["CVE_data_meta"]["ID"]
+                cve_id = v["cve"]["CVE_data_meta"]["ID"]
                 #puts "CVE: #{cve_id}"
 
                 # Get the CWE if we have it
-                if cve["problemtype"] && cve["problemtype"]["problemtype_data"].first
-                  cwe_id = cve["problemtype"]["problemtype_data"].first["description"].first["value"]
+                if v["cve"]["problemtype"] && v["cve"]["problemtype"]["problemtype_data"].first
+                  cwe_id = v["cve"]["problemtype"]["problemtype_data"].first["description"].first["value"]
                   #puts "CWE: #{cwe_id}"
                 end
 
@@ -128,11 +131,11 @@ class Cpe
           end
         end
       end
-
     end
 
     #puts "DEBUG Sending #{vulns.uniq.count} vulns"
-    
+    json = nil
+
   vulns.uniq
   end
 
