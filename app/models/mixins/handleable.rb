02e2fcee12d092e7198364d74fb9d3fcae4646a9
jcran <jcran@pentestify.com>
2018-12-30 23:38:01 -0600

    specify a sychronous handler method

--- app/models/mixins/handleable.rb
+++ app/models/mixins/handleable.rb
@@ -8,6 +8,12 @@ module Handleable
     handler.class.perform_async("#{self.class}", self.id, prefix)
   end
 
+  def handle_synchronous(handler_type, prefix=nil)
+    handler = Intrigue::HandlerFactory.create_by_type(handler_type)
+    handler.perform("#{self.class}", self.id, prefix)
+  end
+
+
   def handle_attached(prefix=nil)
     self.handlers.each do |handler_type|
       handler = Intrigue::HandlerFactory.create_by_type(handler_type)
