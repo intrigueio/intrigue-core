2cbb3c33884fda225f45fda31de1d4c78162b5d3
jcran <jcran@pentestify.com>
2019-03-03 12:47:38 -0600

    comment aws endptoing

--- lib/notifiers/slack.rb
+++ lib/notifiers/slack.rb
@@ -15,6 +15,7 @@ module Notifier
 
       # Assumes amazon...
       if config_hash["system_base_uri"] == "AMAZON"
+        # use the standard endpoint to grab info 
         system_ip = http_get_body("http://169.254.169.254/latest/meta-data/public-ipv4")
         @system_base_uri = "http://#{system_ip}:7777"
       else # use as is
