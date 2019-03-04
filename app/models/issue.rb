7ed1a4b3babe226ec41b731b8f119a73c1008897
jcran <jcran@pentestify.com>
2019-03-04 12:00:47 -0600

    validate unique

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -10,11 +10,17 @@ module Intrigue
       many_to_one  :task_result
       many_to_one  :entity
 
-      self.raise_on_save_failure = true
+      self.raise_on_save_failure = false
 
       def self.scope_by_project(project_name)
         where(:project => Intrigue::Model::Project.first(:name => project_name))
       end
+
+      def validate
+        super
+        validates_unique([:project_id, :type, :name])
+      end
+      
     end
 
   end
