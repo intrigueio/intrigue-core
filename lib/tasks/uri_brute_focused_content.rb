44c5672016c11041305d46a1ec49d20430646fb2
Jonathan Cran <jcran@pentestify.com>
2019-03-28 09:57:49 -0500

    fix stupid false positive

--- lib/tasks/uri_brute_focused_content.rb
+++ lib/tasks/uri_brute_focused_content.rb
@@ -201,7 +201,7 @@ class UriBruteFocusedContent < BaseTask
       { path: '/xmlrpc.php', severity: 5, status: "confirmed", regex: /XML-RPC server accepts POST requests only./ },
       # TODO - look for "1.3.9.1" to disprove vulnerability 
       { path: '/wp-content/plugins/easy-wp-smtp/readme.txt', severity: 1,  regex: /Easy WP SMTP/i, status: "potential" },  
-      { path: '/wp-content/plugins/easy-wp-smtp/css/style.css', severity: 1,  regex: /Settings/i, status: "potential" },  
+      { path: '/wp-content/plugins/easy-wp-smtp/css/style.css', severity: 1,  regex: /swpsmtp_settings_form/i, status: "potential" },  
       { path: '/wp-content/plugins/easy-wp-smtp/', severity: 1,  regex: /debug_log/i, status: "potential" },
       { path: '/wp-content/plugins/easy-wp-smtp/inc/', severity: 1,  regex: /debug_log/i, status: "potential" }
     ] 
