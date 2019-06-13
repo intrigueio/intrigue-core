783b36772591a265a547f54f48c4380c0e0fb200
Jonathan Cran <jcran@pentestify.com>
2019-06-12 23:51:38 -0500

    simplify output, don't require it to be loaded into memory

--- lib/initialize/json_export_file.rb
+++ lib/initialize/json_export_file.rb
@@ -1,20 +1,20 @@
 
 # usage
 #
-# j = JsonExportFile.new
-# j.store_entity {test: 'test'} 
+# j = JsonExportFile.new("/tmp/file.json")
 # j.store_entity {test2: 'test2'} 
-# j.write_and_close ("/tmp/file.json")
+# j.write_and_close 
 #
 #
 class JsonExportFile
 
-  def initialize
-    @temp_file = "#{$intrigue_basedir}/tmp/#{rand(100000000000)}.tmp"
+  def initialize(fullpath=nil)
+    @output_file = fullpath || "#{$intrigue_basedir}/tmp/#{rand(100000000000)}.json"
     @entity_count = 0
     @closed = false
-    # prep files
-    _write_and_flush_line @temp_file, "["
+
+    # prep file
+    _write_and_flush_line @output_file, "["
   end
 
   def store_entity(entity)
@@ -22,42 +22,27 @@ class JsonExportFile
     @entity_count+=1
 
     # add to the end of the list
-    _write_and_flush_line @temp_file, "#{entity.to_json},"
+    _write_and_flush_line @output_file, "#{entity.to_json},"
   end
 
-  def write_and_close(path)
+  def write_and_close
     
     # only allow one write
-    return false if @closed
+    return nil if @closed
 
     # remove trailing commas
     if @entity_count > 0
-      File.truncate(@temp_file, File.size(@temp_file) - 2) # new line and comma
+      File.truncate(@output_file, File.size(@output_file) - 2) # new line and comma
     end
 
     # close it 
-    _write_and_flush_line @temp_file, "]"
+    _write_and_flush_line @output_file, "]"
     @closed = true
 
-    f = File.open(path,"w")
-    f.puts _dump_json
-    f.flush
-    f.close
-
-    # clean up 
-    File.delete(@temp_file)
+  @output_file 
   end
 
   private 
-
-    def _dump_json # dump out the hash, closing files as you go
-      {
-        "generated_at" => "#{DateTime.now}",
-        "entity_count" => @entity_count,
-        "entities" => JSON.parse("#{File.open(@temp_file).read}"),
-      }.to_json
-    end
-
 
     def _write_and_flush_line(file,line)
       fe = File.open(file,"a")
