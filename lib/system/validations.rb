edd4d2054878ecd5b8166b167e8e0751b250ce1c
Jonathan Cran <jcran@pentestify.com>
2019-10-06 22:13:32 -0500

    tweak dns regex and remove log commands from scope (todo... log scope choices back to the entity?

--- lib/system/validations.rb
+++ lib/system/validations.rb
@@ -14,7 +14,7 @@ module Validations
   end
 
   def dns_regex 
-    /^[a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-\_]*[a-zA-Z0-9\-\_]\.*[A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-\_]*[A-Za-z\.]$/
+    /(?=^.{1,253}$)(^((?!-)[a-zA-Z0-9-]{0,62}[a-zA-Z0-9]\.)*[a-zA-Z]{1,63}$)/
   end
 
 end
