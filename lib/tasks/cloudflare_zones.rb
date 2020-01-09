7c5892034fddd6c694c3526099f27305f461a7ad
AnasBensalah <bensalah.anas@gmail.com>
2020-01-09 16:29:15 +0100

    Adding cloudfalre Dns records checker

--- lib/tasks/cloudflare_zones.rb
+++ lib/tasks/cloudflare_zones.rb
@@ -32,42 +32,27 @@ class CloudflareZones < BaseTask
 
     headers = {"X-Auth-Email" => email,
                "X-Auth-Key" => key,
-               "Accept" =>  "application/json" ,
-
+               "Accept" =>  "application/json"
            }
 
     # Def cloudflare_connection (email, key, entity_name)
     Cloudflare.connect(key: key, email: email) do |connection|
 
-    # Get all available zones:
-    zones = connection.zones
-    zone = connection.zones.find_by_name(entity_name)
+      # Get all available zones:
+      zones = connection.zones
+      zone = connection.zones.find_by_name(entity_name)
 
-    # Get DNS records for a given zone:
-    dns_records = zone.dns_records
+      # Get DNS records for a given zone:
+      dns_records = zone.dns_records
 
-    # Show some details of the DNS record:
-    dns_record = dns_records.first
+      # Show some details of the DNS record:
+      dns_record = dns_records.first
 
-    # Get the zone id
-    zone_id = dns_record.zone_id
+      # Get the zone id
+      zone_id = dns_record.zone_id
 
-    # Initialize page number
-    page_num = 1
-
-    # Call the cloudflare Api
-    json = call_api zone_id,page_num,headers
-
-    # Check if the result is not empty
-    if json["result"]
-      json["result"].each do |e|
-        _create_entity("DnsRecord", {"name" => e["name"], "content" => e["content"]})
-      end
-    end
-
-    # Handel the paging issue
-    while json["result_info"]["total_pages"] > page_num
-      page_num += 1
+      # Initialize page number
+      page_num = 1
 
       # Call the cloudflare Api
       json = call_api zone_id,page_num,headers
@@ -78,15 +63,29 @@ class CloudflareZones < BaseTask
           _create_entity("DnsRecord", {"name" => e["name"], "content" => e["content"]})
         end
       end
-    end
-    end
+
+      # Handel the paging issue
+      while json["result_info"]["total_pages"] > page_num
+        page_num += 1
+
+        # Call the cloudflare Api
+        json = call_api zone_id,page_num,headers
+
+        # Check if the result is not empty
+        if json["result"]
+          json["result"].each do |e|
+            _create_entity("DnsRecord", {"name" => e["name"], "content" => e["content"]})
+          end
+        end
+      end
+      end
   end #end run
 
   # Call the cloudflare Api function
   def call_api (zone_id,page_num,headers)
     response = http_get_body("https://api.cloudflare.com/client/v4/zones/#{zone_id}/dns_records&page=#{page_num}",nil,headers)
     json = JSON.parse(response)
-    return json
+  return json
   end
 
 end #end class
