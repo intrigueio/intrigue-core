1c75c71368fdc6f51c26e5b54b35fdc0fbe1b5ea
Jonathan Cran <jcran@pentestify.com>
2020-01-12 19:51:41 -0600

    updated description for netscaler bug

--- lib/issues/citrix_netscaler_cve_2019_19871.rb
+++ lib/issues/citrix_netscaler_cve_2019_19871.rb
@@ -11,7 +11,9 @@ class VulnCitrixNetscalerCve201919871 < BaseIssue
       ],
       severity: 1,
       category: "network",
-      description: "A Citrix Netscaler device was found to be vulnerable to an unauthenticated RCE (CVE-2019-19781). The vulnerability was released in Dec 2019 and no patch was released immediately. Remote, Pre-auth Arbitrary Command Execution is possible due to the logic vulnerability.",
+      description: "A remote pre-auth RCE path traversal vulnerability in Citrix NetScaler affecting devices in " + 
+                   "the default configuration. Existence of the vulnerability was announced by Citrix in late Dec 2019 " +
+                   "with no patch immediately released. Exploits are now available and active exploitation has been detected in the wild.", 
       remediation: "No patch is currently available, apply the workarounds provided by Citrix (see references).",
       affected_software: [
         { :vendor => "Citrix", :product => "NetScaler Gateway", :version => "13.0" },
@@ -23,16 +25,20 @@ class VulnCitrixNetscalerCve201919871 < BaseIssue
       references: [
         { type: "description", uri: "https://nvd.nist.gov/vuln/detail/CVE-2019-19781" },
         { type: "description", uri: "https://www.tripwire.com/state-of-security/vert/citrix-netscaler-cve-2019-19781-what-you-need-to-know/" },
+        { type: "description", uri: "https://www.trustedsec.com/blog/critical-exposure-in-citrix-adc-netscaler-unauthenticated-remote-code-execution/"},
+        { type: "description", uri: "https://isc.sans.edu/diary/25686" },
+        { type: "description", uri: "https://www.mdsec.co.uk/2020/01/deep-dive-to-citrix-adc-remote-code-execution-cve-2019-19781/"},
         { type: "remediation", uri: "https://support.citrix.com/article/CTX267027" },
         { type: "remediation", uri: "https://support.citrix.com/article/CTX267679" },
         { type: "remediation", uri: "https://support.citrix.com/article/CTX267027" },
-        { type: "exploitation", uri: "https://www.trustedsec.com/blog/critical-exposure-in-citrix-adc-netscaler-unauthenticated-remote-code-execution/" },
-        { type: "detection", uri: "https://github.com/Neo23x0/sigma/blob/master/rules/web/web_citrix_cve_2019_19781_exploit.yml" }, 
-        { type: "detection", uri: "https://www.reddit.com/r/netsec/comments/en4mmo/multiple_exploits_for_cve201919781_citrix/" },
-        { type: "threat_intelligence", uri: "https://isc.sans.edu/forums/diary/Citrix+ADC+Exploits+are+Public+and+Heavily+Used+Attempts+to+Install+Backdoor/25700/" },
-        { type: "threat_intelligence", uri: "https://www.trustedsec.com/blog/netscaler-remote-code-execution-forensics" }
-      ], 
-      task: "vuln/citrix_netscaler_rce_cve_2019_19781"
+        { type: "detection_rule", uri: "https://github.com/Neo23x0/sigma/blob/master/rules/web/web_citrix_cve_2019_19781_exploit.yml" }, 
+        { type: "detection_rule", uri: "https://www.reddit.com/r/netsec/comments/en4mmo/multiple_exploits_for_cve201919781_citrix/" },
+        { type: "exploit", uri: "https://github.com/trustedsec/cve-2019-19781" },
+        { type: "exploit", uri: "https://www.exploit-db.com/exploits/47901" },
+        { type: "threat_intel", uri: "https://isc.sans.edu/forums/diary/Citrix+ADC+Exploits+are+Public+and+Heavily+Used+Attempts+to+Install+Backdoor/25700/" },
+        { type: "threat_intel", uri: "https://www.trustedsec.com/blog/netscaler-remote-code-execution-forensics" }
+      ],
+      check: "vuln/citrix_netscaler_rce_cve_2019_19781"
     }.merge!(instance_details)
   end
 
