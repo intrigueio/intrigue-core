4c76d206b1acf0b040fe7c7ddbf2f0ebccba1bb6
AnasBensalah <bensalah.anas@gmail.com>
2020-01-16 17:09:31 +0100

    fix error in opendns task

--- lib/tasks/search_opendns.rb
+++ lib/tasks/search_opendns.rb
@@ -29,9 +29,9 @@ class SearchOpenDns < BaseTask
     resolves_to = resolve_names entity_name
     unless resolves_to.first
       _log "No resolution for this record, unable to check"
-      return 
-    endZ
-    
+      return
+    end
+
     # Query opendns nameservers
     nameservers = ['208.67.222.222', '208.67.220.220']
     _log "Querying #{nameservers}"
@@ -45,21 +45,21 @@ class SearchOpenDns < BaseTask
       _log "Resolves to #{res.map{|x| "#{x.to_s}" }}. Seems we're good!"
     else
       source = "OpenDNS"
-      description = "OpenDNS (now Cisco Umbrella) provides protection against threats on the internet such as malware, " + 
+      description = "OpenDNS (now Cisco Umbrella) provides protection against threats on the internet such as malware, " +
         "phishing, and ransomware."
 
       _create_linked_issue("blocked_potentially_compromised", {
         status: "confirmed",
         additional_description: description,
-        source: source, 
+        source: source,
         proof: "Resolved to the following address(es) outside of #{source} (#{nameservers}): #{resolves_to.join(", ")}",
         to_reproduce: "dig #{entity_name} @#{nameservers.first}",
-        references:  
+        references:
           [{type: "remediation", uri: "https://support.opendns.com/hc/en-us/articles/227987347-Why-is-this-Domain-Blocked-or-not-Blocked-" }]
-      })     
-      
-      # Also store it on the entity 
-      blocked_list = @entity.get_detail("detected_malicious") || [] 
+      })
+
+      # Also store it on the entity
+      blocked_list = @entity.get_detail("detected_malicious") || []
       @entity.set_detail("detected_malicious", blocked_list.concat([{source: source}]))
 
     end
