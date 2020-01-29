6c45bad26b81a0d99b1e9dc5f7e8d4b96c5fc7e1
AnasBensalah <bensalah.anas@gmail.com>
2020-01-29 15:12:20 +0100

    adding "search_emerging_threats" task

---
+++ lib/tasks/search_emerging_threats.rb
@@ -0,0 +1,58 @@
+require "open-uri"
+module Intrigue
+module Task
+class SearchBadIps < BaseTask
+
+  def self.metadata
+    {
+      :name => "search_emerging_threats",
+      :pretty_name => "Search Emerging Threats",
+      :authors => ["Anas Ben Salah"],
+      :description => "This task search Emerging Threats blacklist for listed IP address",
+      :references => ["https://rules.emergingthreats.net/"],
+      :type => "discovery",
+      :passive => true,
+      :allowed_types => ["IpAddress"],
+      :example_entities => [
+        {"type" => "IpAddress", "details" => {"name" => "1.1.1.1"}}
+      ],
+      :allowed_options => [],
+      :created_types => []
+    }
+  end
+
+  def run
+    super
+    # Get the IpAddress
+    entity_name = _get_entity_name
+
+    # Get talos Blacklist IP
+    data = open("https://rules.emergingthreats.net/blockrules/compromised-ips.txt").read
+
+    if data == nil
+      _log_error("Unable to fetch Url !")
+      return
+    end
+
+    # Create an issue if an IP found in the Talos IP Blacklist
+    if data.include? entity_name
+       #_create_malicious_ip_issue(entity_name, severity=4)
+       source = "emergingthreats.net"
+       description = "emerginthreats.net is a well reputed blacklist "
+       _create_linked_issue("malicious_ip", {
+         status: "confirmed",
+         additional_description: description,
+         source: source,
+         proof: "This IP was founded related to malicious activities in #{source}",
+         references: []
+       })
+
+      # Also store it on the entity
+       blocked_list = @entity.get_detail("detected_malicious") || []
+       @entity.set_detail("detected_malicious", blocked_list.concat([{source: source}]))
+    end
+  end # end run
+
+end #end class
+end
+end
