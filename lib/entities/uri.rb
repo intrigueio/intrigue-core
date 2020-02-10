65ee6331895b49313003a052f8d66e2a779fb03c
Jonathan Cran <jcran@pentestify.com>
2020-02-09 21:50:07 -0600

    check uri scope based on hostname, and add a few more standard exceptions

--- lib/entities/uri.rb
+++ lib/entities/uri.rb
@@ -2,6 +2,8 @@ module Intrigue
 module Entity
 class Uri < Intrigue::Model::Entity
 
+  include Intrigue::Task::Dns # TODO ... move parse_domain_name up in the heirarchy
+
   def self.metadata
     {
       :name => "Uri",
@@ -33,6 +35,48 @@ class Uri < Intrigue::Model::Entity
     end
 
   out
+  end
+
+
+  ###
+  ### SCOPING
+  ###
+  def scoped?(conditions={}) 
+    return true if self.seed
+
+    ## CHECK IF DOMAIN NAME IS KNOWN
+    # =================================    
+    host = URI.parse(self.name).to_s
+    if !host.is_ip_address?
+      domain_name = parse_domain_name(host)
+      puts "nope" unless self.project.traversable_entity?(domain_name, "Domain")
+    end
+      
+    
+    ### CHECK OUR SEED ENTITIES TO SEE IF THE TEXT MATCHES
+    ######################################################
+    
+    # Check types we'll check for indicators 
+    # of in-scope-ness
+    #
+    scope_check_entity_types = [
+      "Intrigue::Entity::Organization",
+      "Intrigue::Entity::DnsRecord",
+      "Intrigue::Entity::Domain" 
+    ]
+
+    if self.project.seeds
+      self.project.seeds.each do |s|
+        next unless scope_check_entity_types.include? s.type.to_s
+        if domain_name =~ /#{Regexp.escape(s.name)}/i
+          #_log "Marking as scoped: SEED ENTITY NAME MATCHED TEXT: #{s["name"]}}"
+          return true
+        end
+      end
+    end
+
+  # if we didnt match the above and we were asked, it's still true
+  true
   end
 
   def enrichment_tasks
