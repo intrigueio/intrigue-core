178a13aea1f3b688941e23c8ccafea165daa5131
Jonathan Cran <jcran@pentestify.com>
2020-02-29 14:19:50 -0600

    fix issue name

--- lib/issues/gratuitous_external_resources_requested.rb
+++ lib/issues/gratuitous_external_resources_requested.rb
@@ -4,13 +4,13 @@ module Intrigue
   
     def self.generate(instance_details={})
       {
-        name: "c",
+        name: "gratuitous_external_resources_requested",
         pretty_name: "Abnormally Large Number of External Resources",
         severity: 5,
         category: "application",
         description: "When a browser requested the resource, a large number of requests weree made to external hosts. In itself, this may not be a security problem, but can introduce more attack surface than necessary, and is indicative of poor security hygiene, as well as slow load times for a service.",
         remediation: "Investigate the resource, and determine if this behavior is expected.",
-        affected_software: [ ],
+        affected_software: [],
         references: [ # types: description, remediation, detection_rule, exploit, threat_intel
         ]
       }.merge!(instance_details)
