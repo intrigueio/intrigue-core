0e3f52d62d77b93bf7123ec0a2a338946d437599
Jonathan Cran <jcran@pentestify.com>
2020-03-09 20:55:20 -0500

    wordpress vs sharepoint

--- lib/issues/wordpress_config_leak.rb
+++ lib/issues/wordpress_config_leak.rb
@@ -5,20 +5,14 @@ module Intrigue
     def self.generate(instance_details={})
       {
         name: "wordpress_config_leak",
-        pretty_name: "Microsoft Sharepoint Info Leak",
+        pretty_name: "Wordpress Configuraton Info Leak",
         severity: 1,
         category: "application",
         status: "confirmed",
         description: "A wordpress site was found with an exposed configuration.",
-        remediation: "Remove the exposed configuration file.",
-        affected_software: [ 
-          { :vendor => "Wordpress", :product => "Wordpress" }
-          ],
+        remediation: "Set permissions on the configuration file to prevent anonymous users being able to read it.",
+        affected_software: [{ :vendor => "Wordpress", :product => "Wordpress" }],
         references: [ # types: description, remediation, detection_rule, exploit, threat_intel
-          { type: "description", uri: "https://exchange.xforce.ibmcloud.com/vulnerabilities/31642" },
-          { type: "remediation", uri: "https://sharepoint.stackexchange.com/questions/11504/sharepoint-2010-disable-hide-references-to-spsdisco-aspx" },
-          { type: "remediation", uri: "https://social.msdn.microsoft.com/Forums/vstudio/en-US/df090d4b-ba9b-4212-a524-e3e2bb50cacc/hiding-asmxwsdl?forum=wcf" }
-        ]
       }.merge!(instance_details)
     end
   
