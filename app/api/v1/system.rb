b06f422e97ac0aa509dedef647b987ae62df0f5c
Jonathan Cran <jcran@pentestify.com>
2020-03-20 11:07:50 -0500

    flesh out endpoint for specific task's metadata, clean up response to be more RESTful

--- app/api/v1/system.rb
+++ app/api/v1/system.rb
@@ -1,22 +1,41 @@
 class IntrigueApp < Sinatra::Base
 
+  def wrapped_api_response(error, result=nil)
+    success = error.nil?
+  {success: success, error: error, result: result}.to_json
+  end
+
   # Export All Task Metadata
   get "/api/v1/tasks" do 
     content_type 'application/json'
-    Intrigue::TaskFactory.list.map{ |t| t.metadata }.sort_by{|m| m[:name] }.to_json
+    tasks_metadata = Intrigue::TaskFactory.list.map{ |t| t.metadata }.sort_by{|m| m[:name] }
+  wrapped_api_response(nil, tasks_metadata)
   end
 
   # Export All Entity Types
   get "/api/v1/entities" do 
     content_type 'application/json'
-    Intrigue::EntityFactory.entity_types.map{ |e| e.metadata }.sort_by{|m| m[:name] }.to_json
+    entity_metadata = Intrigue::EntityFactory.entity_types.map{ |e| e.metadata }.sort_by{|m| m[:name] }
+  wrapped_api_response(nil, entity_metadata)
   end
 
   # Export a specific Task's metadataa
-  #get "/api/v1/tasks/:task_name" do 
-  #  content_type 'application/json'
-  #  task_name = params[:splat][0..-1].join('/')
-  #  Intrigue::TaskFactory.list.select{|t| t.metadata[:name] == task_name }.first.metadata.to_json
-  #end 
+  get "/api/v1/tasks/:task_name" do 
+    content_type 'application/json'
+    task_name = params[:task_name]
+    
+    # Attempg to get the task
+    task = Intrigue::TaskFactory.list.select{|t| t.metadata[:name] == task_name }.first
+    task_metadata = task.metadata if task
+
+    unless task_metadata
+      status 400
+      return wrapped_api_response("unable to find task with that name")
+    end
+  
+    wrapped_api_response(nil, task_metadata)
+  end 
 
 end
+
+
