7eae6c2501d88b5406cccf1d6dbf05613e99cb71
AnasBensalah <bensalah.anas@gmail.com>
2020-05-28 00:37:53 +0100

    Search IPQualityScore for IP reputation (fraud score and nature TOR,VPN,Proxy, ...)

---
+++ lib/tasks/threat/search_ipqs.rb
@@ -0,0 +1,73 @@
+module Intrigue
+module Task
+class SearchIPQS < BaseTask
+
+
+  def self.metadata
+    {
+      :name => "threat/search_IPQualityScore",
+      :pretty_name => "Threat Check - Search IPQualityScore",
+      :authors => ["Anas Ben Salah"],
+      :description => "This task hits IPQualityScore.com api for IP reputation and nature",
+      :references => ["https://www.ipqualityscore.com/documentation/proxy-detection/overview"],
+      :type => "threat_check",
+      :passive => true,
+      :allowed_types => ["IpAddress"],
+      :example_entities => [{"type" => "IpAddress", "details" => {"name" => "1.1.1.1"}}],
+      :allowed_options => [],
+      :created_types => []
+    }
+  end
+
+
+  ## Default method, subclasses must override this
+  def run
+    super
+
+      #get entity name and type
+      entity_name = _get_entity_name
+
+      #get keys for API authorization
+      password =_get_task_config("ipqs_api_key")
+
+      headers = { "Accept" =>  "application/json"}
+
+      unless password
+        _log_error "unable to proceed, no API key for IpQulaityScore provided"
+        return
+      end
+
+      # Get responce
+      response = http_get_body("https://www.ipqualityscore.com/api/json/ip/#{password}/#{entity_name}?strictness=0&allow_public_access_points=true&fast=true&lighter_penalties=true&mobile=true",nil, headers)
+      result = JSON.parse(response)
+      #puts result
+
+      #Create organization entity
+      if response["organization"]
+        _create_entity("Organization", {"name" => response["organization"]})
+      end
+
+      #Create PhysicalLocation entity
+      if response["region"]
+        _create_entity("PhysicalLocation", {"name" => response["region"]})
+      end
+
+      #create an issue if this IP related to fraud
+      if result["success"] and result["fraud_score"] > 0
+        _create_linked_issue("suspicious_activity_detected", {
+          status: "confirmed",
+          description: "This ip was flagged by IPQulatiyScore for fraud activites",
+          IpQulaityScore_details: result,
+          source: "IpQulaityScore.com"
+        })
+        # Also store it on the entity
+        blocked_list = @entity.get_detail("suspicious_activity_detected") || []
+        @entity.set_detail("suspicious_activity_detected", blocked_list.concat([{}]))
+    end
+  end #end run
+
+
+
+end
+end
+end
