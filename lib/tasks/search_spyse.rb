4b79f51764f790fd4d2bb1f0e94f53a85acf616d
AnasBensalah <bensalah.anas@gmail.com>
2020-06-09 02:15:57 +0100

    adding search spyse task for IP reputation and checking open ports

--- lib/tasks/search_spyse.rb
+++ lib/tasks/search_spyse.rb
@@ -33,58 +33,36 @@ class SearchSpyse < BaseTask
     if entity_type == "IpAddress"
       #search Ip for reputation, Open ports, related information
       search_ip_reputation entity_name,headers
+      search_open_ports entity_name,headers
     else
       _log_error "Unsupported entity type"
     end
 
-  end #end run
+  end #end
 
-  #Lists open ports on IP
-  # def search_ip_port(entity_name, headers)
-  #
-  #   # Set the headers
-  #   url = "https://api.spyse.com/v2/data/ip/port?limit=100&ip=#{entity_name}"
-  #
-  #   # make the request
-  #   response = http_get_body(url,nil,headers)
-  #   json = JSON.parse(response)
-  #   #puts json
-  #   json["data"]["items"].each do |result|
-  #       puts "#{entity_name}: #{result["port"]}"
-  #     if result["service"] == ""
-  #
-  #     #_create_entity("IpAddress", {"name" => entity_name, "open_port" => result["port"] , "service" => result["service"]})
-  #       _log_good "Creating service on #{entity_name}: #{result["port"]}"
-  #       _create_network_service_entity(@entity, result["port"])
-  #     else
-  #       _log_good "Creating service on #{entity_name}: #{result["port"]}"
-  #       _create_network_service_entity(@entity, result["port"],result["service"])
-  #     end
-  #   end
-  # end
-
-  #search IP
+  # Search IP reputation and gathering data
   def search_ip_reputation(entity_name, headers)
 
-    # Set the headers
+    # Set the URL for ip data
     url = "https://api.spyse.com/v2/data/ip?limit=100&ip=#{entity_name}"
 
     # make the request
     response = http_get_body(url,nil,headers)
     json = JSON.parse(response)
-    #puts json
-    json["data"]["items"].each do |result|
-      # #create physical location
-      # if result["maxmind_geo"]["country"]
-        # puts result["maxmind_geo"]["country"]
-         #_create_entity("PhysicalLocation", "name" => result["maxmind_geo"]["country"])
-      # end
-      # # Create list of related organizations
-      # if result["maxmind_isp"]["org"]
-         #puts result["maxmind_isp"]["org"]
-         #_create_entity("Organization", "name" => result["entity"]["organization"])
-      # end
 
+    json["data"]["items"].each do |result|
+
+      # Create physical location
+      if result["maxmind_geo"]["country"]
+        _create_entity("PhysicalLocation", "name" => result["maxmind_geo"]["country"], "extended_spyse" => result["maxmind_geo"])
+      end
+
+      # Getting ISP organization
+      if result["maxmind_isp"]["org"]
+        _create_entity("Organization", "name" => result["maxmind_isp"]["org"], "extended_spyse" => result["maxmind_isp"])
+      end
+
+      # Create an issue if result score indicates a score related to the threat
       if result["score"] < 100
         _create_linked_issue("suspicious_activity_detected",{
           severity: result["score"],
@@ -94,23 +72,23 @@ class SearchSpyse < BaseTask
         })
       end
     end
-    # Search for open ports
+  end
+
+  # Search for open ports
+  def search_open_ports entity_name, headers
+
+    # Set the URL for ip open ports
     url2 = "https://api.spyse.com/v2/data/ip/port?limit=100&ip=#{entity_name}"
 
     # make the request
     response2 = http_get_body(url2,nil,headers)
     json2 = JSON.parse(response2)
-    #puts json
+
     json2["data"]["items"].each do |result|
-        puts "#{entity_name}:#{result["port"]}"
-      #_create_entity("IpAddress", {"name" => entity_name, "open_port" => result["port"] , "service" => result["service"]})
-        _log_good "Creating service on #{entity_name}: #{result["port"]}"
-        _create_network_service_entity(@entity, result["port"])
+      _log_good "Creating service on #{entity_name}: #{result["port"]}"
+      _create_network_service_entity(@entity, result["port"],protocol="tcp",generic_details={"extended_spyse" => result})
     end
   end
-
-  
-
 
 end
 end
