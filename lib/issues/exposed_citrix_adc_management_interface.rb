40ea299d126692d432a214dd57dfedfaba2617ab
Jonathan Cran <jcran@pentestify.com>
2020-07-16 09:23:28 -0500

    add new issues

--- lib/issues/exposed_citrix_adc_management_interface.rb
+++ lib/issues/exposed_citrix_adc_management_interface.rb
@@ -4,18 +4,15 @@ module Intrigue
   
     def self.generate(instance_details={})
       {
-        name: "exposed_bigip_configuration_utility",
-        pretty_name: "Exposed BIG-IP Configuration Utility",
+        name: "exposed_citrix_adc_management_interface",
+        pretty_name: "Exposed Citrix ADC Management Interface",
         severity: 2, 
         category: "network",
         status: "confirmed",
-        description: "The F5 BigIP configutation utility was discovered. This should never be exposed to the data networks, and should be checked for CVE-2020-5902",
+        description: "The Citrix ADC Management interface  was discovered. This should never be exposed to the data networks, and should be checked for vulnerabilties",
         remediation: "Prevent access to this panel by placing it behind a firewall or otherwise restricting access.",
-        #affected_software: [
-        #  { :vendor => "F5", :product => "Big-IP Configuration Utility" },
-        #],
         references: [ # types: description, remediation, detection_rule, exploit, threat_intel
-          {:type => "threat_intel", :uri => "https://twitter.com/n0x08/status/1278812795031523328"}
+          {:type => "threat_intel", :uri => "https://blog.unauthorizedaccess.nl/2020/07/07/adventures-in-citrix-security-research.html"}
         ],
         # task: handled in ident
       }.merge!(instance_details)
