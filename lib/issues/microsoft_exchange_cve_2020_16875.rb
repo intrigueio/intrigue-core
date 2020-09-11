956b898c02fec62b7fefb6f907c88583fa6e5434
Jonathan Cran <jcran@pentestify.com>
2020-09-11 17:14:25 -0500

    adjust issue id

--- lib/issues/microsoft_exchange_cve_2020_16875.rb
+++ lib/issues/microsoft_exchange_cve_2020_16875.rb
@@ -5,12 +5,12 @@ module Intrigue
     def self.generate(instance_details={})
       {
         added: "2020-09-11",
-        name: "microsoft_exchange_cve_2020_0688",
-        pretty_name: "Microsoft Exchange Server DlpUtils AddTenantDlpPolicy Remote Code Execution Vulnerabilit (CVE-2020-16875)",
+        name: "microsoft_exchange_cve_2020_16875",
+        pretty_name: "Microsoft Exchange Server DlpUtils AddTenantDlpPolicy Remote Code Execution Vulnerability (CVE-2020-16875)",
         identifiers: [
           { type: "CVE", name: "CVE-2020-16875" }
         ],
-        severity: 1,
+        severity: 2,
         status: "potential",
         category: "vulnerability",
         description: "This vulnerability allows remote attackers to execute arbitrary code on affected installations of Exchange Server. Authentication is required to exploit this vulnerability. The specific flaw exists within the processing of the New-DlpPolicy cmdlet. The issue results from the lack of proper validation of user-supplied template data when creating a dlp policy. An attacker can leverage this vulnerability to execute code in the context of SYSTEM.",
