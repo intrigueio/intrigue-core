525533d8ad25ebb2a4f85437b3c6c879eb96233c
Jonathan Cran <jcran@pentestify.com>
2020-11-30 08:05:07 -0600

    fixup CVE-2020-2021 naming

--- lib/issues/paloalto_globalprotect_cve_2020_2021.rb
+++ lib/issues/paloalto_globalprotect_cve_2020_2021.rb
@@ -5,7 +5,7 @@ module Intrigue
     def self.generate(instance_details={})
       {
         added: "2020-07-03",
-        name: "paloalto_globalprotect_cve_2019_2021",
+        name: "paloalto_globalprotect_check_cve_2020_2021",
         pretty_name: "PaloAlto GlobalProtect Vulnerability (CVE-2020-2021)",
         identifiers: [
           { type: "CVE", name: "CVE-2020-2021" }
