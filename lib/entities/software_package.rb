442d2bde1712847c79364c7b948f1d481a0e8284
Jonathan Cran <jcran@pentestify.com>
2020-12-10 12:23:18 -0600

    adjust regexes

--- lib/entities/software_package.rb
+++ lib/entities/software_package.rb
@@ -12,7 +12,7 @@ class SoftwarePackage < Intrigue::Core::Model::Entity
   end
 
   def validate_entity
-    name =~ /^[[:word:]]+$/
+    name =~ /^[[[:word:]]\,\s\(\)\[\]]+$/
   end
 
   def detail_string
