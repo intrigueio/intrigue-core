0b4899860206e52723194d84744420a18129a2b3
Shpend Kurtishaj <shpendk13@gmail.com>
2020-12-16 23:26:48 +0100

    only make postgres/redis changes if /data directory is present

--- util/intriguectl.sh
+++ util/intriguectl.sh
@@ -27,24 +27,8 @@ Help()
     echo "start   start intrigue services"
     echo "stop    stop intrigue services"
     echo
-    echo "To set your basic auth password, export variable CORE_PASSWD and (re)run setup"
+    echo "To set your basic auth password, export variable INTRIGUE_PASS and (re)run setup"
     echo
-}
-
-SetPassword()
-{
-    if [ "${CORE_PASSWD}" ]; then
-      FILE1=~/core/config/config.json
-      FILE2=~/core/config/config.json.default
-      if [ -f "$FILE1" ]; then
-          sed -i "s/\"password\": \".*\"/\"password\": \"${CORE_PASSWD}\"/g" $FILE1
-      elif [ -f "$FILE2" ]; then
-          sed -i "s/\"password\": \".*\"/\"password\": \"${CORE_PASSWD}\"/g" $FILE2
-      else
-          echo "[!] Failed to set basic auth password. Check that your installation is in your home directory"
-      fi
-    fi
-    
 }
 
 Setup()
@@ -53,34 +37,11 @@ Setup()
     if [ "${WORKER_CONFIG}" ]; then
       echo "[+] We are a worker-only configuration!"
       return
-    fi 
-    
-    # set password
-    SetPassword
-
-    # Set up database if it's not already there 
-    if [ ! -d /data/postgres ]; then
-      echo "[+] Configuring postgres..."
-      sudo service postgresql stop
-      sudo mkdir -p /data/postgres
-      sudo chown postgres:postgres /data/postgres 
-      sudo -u postgres /usr/lib/postgresql/*/bin/initdb /data/postgres 
-      sudo service postgresql start
-    fi 
+    fi
 
     # force user/db creation, just in case
     sudo -u postgres createuser intrigue 2> /dev/null
     sudo -u postgres createdb intrigue_dev --owner intrigue 2> /dev/null
-
-    # Adjust and spin up redis
-    if [ ! -d /data/redis ]; then
-      echo "[+] Configuring redis..."
-      sudo service redis-server stop
-      sudo mkdir -p /data/redis
-      sudo chown redis:redis /data/redis
-      sudo service redis-server start
-      sudo systemctl daemon-reload
-    fi
 
     # change to core's directory
     cd ~/core
@@ -104,31 +65,29 @@ Setup()
 # Main program                                                                 #
 ################################################################################
 ################################################################################
-Welcome
 
 cmd=$1
 
 if [ -z "$cmd" ];
 then
+    Welcome
     Help
     exit
 fi
 
 if [ "$cmd" == "start" ]; then
+    Welcome
     echo "[+] Starting intrigue..."
-    # set password
-    SetPassword
-
     # start services
     cd ~/core
-    god start
+    god start > /dev/null
     sleep 25
     ip=$(ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
     echo "Browse to https://$ip:7777 and login with user 'intrigue' and the given or pregenerated password"
 elif [ "$cmd" == "stop" ]; then
     echo "[+] Stopping intrigue..."
     cd ~/core
-    god stop
+    god stop > /dev/null
 elif [ "$cmd" == "setup" ]; then
     echo "[+] Initializing intrigue..."
     Setup
