0a3a6085549df49fb3dca45e813266fa35a02d08
jcran <jcran@intrigue.io>
2020-12-20 23:39:36 -0600

    modularize parseable file formats

--- app/routes/results.rb
+++ app/routes/results.rb
@@ -86,6 +86,8 @@ class CoreApp < Sinatra::Base
     # Helper to construct the request to the API when the application is used interactively
     post '/:project/interactive/single/?' do
 
+      
+
       task_name = "#{@params["task"]}"
       entity_id = @params["entity_id"]
       depth = @params["depth"].to_i
@@ -196,100 +198,26 @@ class CoreApp < Sinatra::Base
 
       # get the file
       entity_file = @params["entity_file"]["tempfile"]
-      f = File.open entity_file,"r"
-      file_lines = f.readlines
-      f.close
-
-      # ensure we're sane  with the data we're bringing in
-      file_lines.each do |l|
-        unless l =~ /[\w\d\s\_\-\:\\\/\#\.]+/ # check for entity sanity
-          session[:flash] = "Unacceptable entity: #{l}, failing"
-          redirect FRONT_PAGE
-        end
-      end
-
 
       ###
       ### Standard file type (entity list)
       ###
-      entities = []
+
       # handle file if we got it
       if file_format == "entity_list"
-        puts 'Parsing Standard entity file'
-        file_lines.each do |l|
-          next if l[0] == "#" # skip comment lines
-
-          # strip out the data
-          et, en = l.split(",").map{|x| x.strip}
-
-          entities << {entity_type: "#{et}", entity_name: "#{en}", }
-        end
-      ###
+        entities = core_csv_to_entities(entity_file)
       ### Intrigue.io Bulk FP
-      ###
       elsif file_format == "intrigueio_fingerprint_csv"
-        puts 'Parsing Intrigue.io Bulks Fingerprint file'
-        file_lines.each do |l|
-
-          next if l =~ /^collection, entity type, entity name/i
-
-          # strip out the data
-          split_line = l.split(",").map{|x| x.strip }
-          col = split_line[0] # indicator type
-          et = split_line[1] # indicator
-          en = split_line[2] # indicator
-
-          entities << {collection: col, entity_type: "#{et}", entity_name: "#{en}"}
-        end
-      ###
+        entities = intrigueio_csv_to_entities(entity_file)
       ### Alienvault OTX (CSV)
-      ###
       elsif file_format == "otx_csv"
-        puts 'Parsing Alienvault file'
-        file_lines.each do |l|
-
-          next if l =~ /^Indicator type,Indicator,Description\r\n$/
-
-          # strip out the data
-          split_line = l.split(",").map{|x| x.strip }
-          et = split_line[0] # indicator type
-          en = split_line[1] # indicator
-
-          # start here
-          modified_et = et.capitalize
-
-          # translate
-          modified_et = "Uri" if modified_et == "Url"
-          modified_et = "DnsRecord" if modified_et == "Hostname"
-          modified_et = "IpAddress" if modified_et == "Ipv4"
-          modified_et = "IpAddress" if modified_et == "Ipv6"
-
-          entities << {entity_type: "#{modified_et}", entity_name: "#{en}" }
-        end
-        ###
-        ### Shodan.io (CSV)
-        ###
-        elsif file_format == "shodan_csv"
-          puts 'Parsing shodan file'
-          file_lines.each do |l|
-
-            next if l =~ /^IpAddress,Indicator\r\n$/
-
-            # strip out the data
-            split_line = l.split(",").map{|x| x.strip }
-            et = split_line[0] # indicator type.
-            en = split_line[1] # indicator
-
-
-            # start here
-            modified_et = et.capitalize
-
-            # translate
-            modified_et = "IpAddress" if modified_et == "Ipv4"
-            modified_et = "IpAddress" if modified_et == "Ipv6"
-
-            entities << {entity_type: "#{modified_et}", entity_name: "#{en}" }
-          end
+        entities = alienvault_otx_csv_to_entities(entity_file)
+      ### BinaryEdge (JSON)
+      elsif file_format == "binary_edge_json"
+        entities = binary_edge_json_to_entities(entity_file)
+      ### Shodan.io (CSV)
+      elsif file_format == "shodan_csv"
+        entities = shodan_csv_to_entities(filename)
       else
         session[:flash] = "Unkown File Format #{file_format}, failing"
         redirect FRONT_PAGE
