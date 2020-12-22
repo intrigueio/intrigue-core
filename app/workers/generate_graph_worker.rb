1371df56a938d5136f4198a52411c0933d78ff72
jcran <jcran@intrigue.io>
2020-12-22 14:08:59 -0600

    tweak graph view to display entities with issues differnetly

--- app/workers/generate_graph_worker.rb
+++ app/workers/generate_graph_worker.rb
@@ -29,6 +29,7 @@ class GenerateGraphWorker
 
   end
 
+
   def generate_graph(project)
     # generate the nodes
     nodes = []
@@ -37,32 +38,55 @@ class GenerateGraphWorker
 
     project.task_results.each do |t|
 
-      #next unless t.base_entity.type_string == "IpAddress"
-
       # add the base entity first (provided it hasn't been deleted)
-      x = { :id => t.base_entity.id, :label => "#{t.base_entity.name}", :type => t.base_entity.type_string}
-      nodes << x unless t.base_entity.deleted?
+      b = t.base_entity
 
+      unless b.deleted?
+        x = { 
+          :id => b.id, 
+          :label => "#{b.name}", 
+          :type => b.type_string }
+
+          # set the color
+          x[:color] = "#ffffa0" if b.issues.count > 0
+          x[:color] = "#ff0000" if b.issues.count > 0 && b.issues.find{|i| i.severity < 3 }
+          x[:color] = "#808080" unless b.scoped
+
+        nodes << x
+      end
+    
       # then for each of the entities, generate the node and edges. skip if deleted.
       t.entities.each do |e|
+        next if e.deleted?
 
-        #next unless e.type_string == "IpAddress"
+        x = { 
+          :id => e.id, 
+          :label => "#{e.name}", 
+          :type => e.type_string } 
 
-        x = { :id => e.id, :label => "#{e.name}", :type => e.type_string } #unless e.secondary
-        #x[:color] = "lightgrey" if e.type_string == "Uri"
+        # set the color
+        x[:color] = "#ffffa0" if e.issues.count > 0
+        x[:color] = "#ff0000" if e.issues.count > 0 && e.issues.find{|i| i.severity < 3 }
+        x[:color] = "#808080" unless e.scoped
 
-        nodes << x unless e.deleted?
+        nodes << x 
 
-        unless t.base_entity.deleted? || e.deleted?
-          edges << {"id" => edge_count += 1, "source" => t.base_entity.id, "target" => e.id}
+        unless t.base_entity.deleted?
+          edges << { 
+            "id" => edge_count += 1, 
+            "source" => b.id, 
+            "target" => e.id
+          }
         end
 
       end
+      
     end
 
-    # dump the json
-    { "nodes" => nodes.uniq!, "edges" => edges }
+  # dump the json
+  { "nodes" => nodes.uniq{|x| x[:id] }, "edges" => edges }
   end
+
 
 
 
