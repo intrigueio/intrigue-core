8d0dbe4ff41e0d3a7e4129ad59006b1066f44d5c
AnasBensalah <35723779+AnasBensalah@users.noreply.github.com>
2020-12-24 23:12:12 +0100

    adding multi thread handling to search azure blob

--- lib/tasks/search_azure_blob.rb
+++ lib/tasks/search_azure_blob.rb
@@ -7,17 +7,16 @@ class SearchAzureBlob < BaseTask
       :name => "search_azure_blob",
       :pretty_name => "Search Azure Blob",
       :authors => ["Anas ben salah"],
-      :description => "This task takes a string or Domain name to determine if there is any exposed Azure blob and lists its files if possible",
+      :description => "This task takes a UniqueKeyword or Domain name to determine if there is any exposed Azure blob and lists its files if possible",
       :references => [],
       :type => "Discovery",
       :passive => true,
-      :allowed_types => ["String","Domain"],
+      :allowed_types => ["UniqueKeyword","Domain"],
       :example_entities => [
-        {"type" => "String", "details" => {"name" => "intrigue"}},
+        {"type" => "UniqueKeyword", "details" => {"name" => "intrigue"}},
         {"type" => "Domain", "details" => {"name" => "intrigue.io"}}
       ],
       :allowed_options => [
-        #{:name => "use_file", :regex => "boolean", :default => false }
       ],
       :created_types => ["AzureBlob"]
     }
@@ -35,34 +34,33 @@ class SearchAzureBlob < BaseTask
     blob_url = 'blob.core.windows.net'
 
     # Lists
-    account_brute_list = []
     valid_account_list = []
-    container_list = []
 
     # get entity details
     if entity_type == "Domain"
       account_name = account_name.split('.')[0]
       _log "Searching results for #{account_name} ..."
-    elsif entity_type == "String"
+    elsif entity_type == "UniqueKeyword"
       _log "Searching results for #{account_name} ..."
     else
       _log_error "Unsupported entity type"
     end
 
     # Generate blob brute force list
-    account_brute_list = brute_force_list account_name
     # Brute force Azure server, get valid accounts for open Blobs and create issues
-    valid_account_list = brute_force_blob account_brute_list, blob_url
+    valid_account_list = brute_force_blob account_name, blob_url
+
     # Generate container brute force list
-    container_list = container_brute_list valid_account_list
-    # Brute force Azure server for listing exposed files in containers and create issues
-    brute_force_container container_list
+    # Brute force Azure cntainers for listing exposed files and create issues
+    brute_force_container valid_account_list
 
   end # End run
 
-  #Checking for the blob response using a list of potential words
-  def check_blob_response (blob_uri)
-    response = http_request :get, blob_uri
+
+
+  #Checking for the blob response 
+  def check_blob_response (response)
+    
     # Dealing with types of responses
     if (response.code=="0")
       return
@@ -74,106 +72,151 @@ class SearchAzureBlob < BaseTask
       _log "Disabled Storage Account"
     elsif (response.reason.include? "Value for one of the query")
       _log "HTTP-OK Storage Account"
-      #_create_entity("AzureBlob", {"account_name" => "#{account_name}","uri" => "#{blob_uri}"})
       #Create an issue if we have a positive response for an open blob
       _create_linked_issue("azure_blob_open_publicly", {
         status: "confirmed",
-        detailed_description: "This url: #{blob_uri} can be accessed publicly",
+        detailed_description: "This url: #{response.request.base_url} can be accessed publicly",
         details: {
-          uri: blob_uri
+          uri: response.request.base_url
           }
       })
       #Extracting valid uri for open blob
-      valid_uri = blob_uri
+      valid_uri = response.request.base_url
     elsif (response.reason.include? "The account being accessed")
       _log "HTTPS-Only Storage Account"
-      #_create_entity("AzureBlob", {"account_name" => "#{account_name}","uri" => "#{blob_uri}"})
       #Create an issue if we have a positive response for an open blob
       _create_linked_issue("azure_blob_open_publicly", {
         status: "confirmed",
-        detailed_description: "This url: #{blob_uri} can be accessed publicly",
+        detailed_description: "This url: #{response.request.base_url} can be accessed publicly",
         details: {
-          uri: blob_uri
+          uri: response.request.base_url
           }
       })
       #Extracting valid uri for open blob
-      valid_uri  = blob_uri
+      valid_uri  = response.request.base_url
     else
       return
-    end #end if location
+    end #end if 
 
-    return valid_uri
-  end
-
-  #Create a brute list combining user input and permutations list
-  def brute_force_list (account_name)
-    account_brute_list=["#{account_name}"]
-    file = File.open "#{$intrigue_basedir}/data/permutations.json"
-    permut = JSON.load file
-    permut["permutations"].each do |p|
-      # account_name + keyword
-      account = "#{account_name}"+"#{p}"
-      account_brute_list.append(account)
-      # keyword + account_name
-      account = "#{p}"+"#{account_name}"
-      account_brute_list.append(account)
-    end
-
-    return account_brute_list
+    return response.request.base_url
   end
 
   # brute force azure server for checking valid blob url
-  def brute_force_blob (account_brute_list, blob_url)
-    valid_account_list = []
-    account_brute_list.each do |account|
-      blob_uri = "https://"+"#{account}"+"."+"#{blob_url}"
-      valid = check_blob_response blob_uri
-      if valid != nil
-        valid_account_list.append(valid)
+  def brute_force_blob (account_name, blob_url)
+    # initializing lists
+    valid_account_list=[]
+    list_to_bruteforce=["#{account_name}"]
+
+    # Load permutation wordlist
+    file = File.open "#{$intrigue_basedir}/data/permutations.json"
+    permut = JSON.load file
+
+    # Combining user input and permutations list
+    permut["permutations"].each do |p|
+      # account_name + keyword
+      account = "#{account_name}"+"#{p}"
+      list_to_bruteforce.append(account)
+      # keyword + account_name
+      account = "#{p}"+"#{account_name}"
+      list_to_bruteforce.append(account)
+    end
+
+    # initializing Queue for handling multi-thread requests 
+    work_q = Queue.new
+
+    # push everything into queue
+    list_to_bruteforce.each do |b|
+      blob_uri = "https://"+"#{b}"+"."+"#{blob_url}"
+      work_q.push(blob_uri)
+    end
+
+    # run requests and get responses
+    responses = make_http_requests(work_q, 20)
+
+    # Creating a list of valid responses   
+    responses.each do |r| 
+      is_valid_url = check_blob_response r
+      if is_valid_url
+        valid_account_list.append(is_valid_url)
       end
     end
+
     #return a list of valid accounts
     return valid_account_list
+
   end
 
+  #handling multithread respones 
+  def make_http_requests(work_q, threads=1)
+    # Create a pool of worker threads to work on the queue
+    responses = []
+    workers = (0...threads).map do
+      Thread.new do
+        begin
+          #_log "Getting request"
+          while request_uri = work_q.pop(true)
+            result = http_request :get, request_uri
+            responses.append(result)
+          end # end while
+        rescue ThreadError
+        end
+      end
+    end; "ok"
+    workers.map(&:join); "ok"
 
-  #Create a brute list combining valid blob uri and container_brute_force list
-  def container_brute_list(valid_account_list)
+    return responses
+  end
+
+  # Create a brute list combining valid blob uri and container_brute_force list
+  # brute force azure container for extracting files
+  def brute_force_container(valid_account_list)
+    # initializing list
     container_brute_list=[]
+    
+    # initializing Queue for handling multi-thread requests
+    work_q = Queue.new
+
+    # Load permutation wordlist
     file = File.open "#{$intrigue_basedir}/data/container_brute_force.json"
     container_list = JSON.load file
+
+    # Combining valid account list and permutations list
     valid_account_list.each do |blob_valid_uri|
       container_list["container"].each do |container_name|
         container_uri = "#{blob_valid_uri}"+"/#{container_name}/?comp=list"
         container_brute_list.append(container_uri)
       end
     end
-    return container_brute_list
-  end
 
-
-  # brute force azure server for extracting files
-  def brute_force_container (container_brute_list)
-    container_brute_list.each do |container_uri|
-      check_container_response container_uri
+    # push everything into queue
+    container_brute_list.each do |c|
+      work_q.push(c)
     end
+
+    # return multiples responses 
+    responses = make_http_requests(work_q, 20)
+
+    # Brute force azure container for extracting files
+    responses.each do |r|
+      check_container_response r 
+    end 
   end
+
 
   # Checking for the container_uri response
-  def check_container_response (container_uri)
-    response = http_request :get, container_uri
+  def check_container_response (response)
     # Dealing with types of responses
     if (response.code=="404")
       return
     elsif (response.code=="200")
-      all_files = list_container_content container_uri
+      all_files = list_container_content response.request.base_url
       #Create an issue if we have a positive response for container uri and exposed files
       _create_linked_issue("azure_blob_exposed_files", {
         status: "confirmed",
-        detailed_description: "This container #{container_uri} can be accessed publicly and it exposes files",
+        detailed_description: "This container #{response.request.base_url} can be accessed publicly and it exposes files",
         details: {
           exposed_files: all_files,
-          uri: container_uri
+          uri: response.request.base_url
           }
       })
     end
