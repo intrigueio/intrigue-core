b8ecc90766139781b38bfb29ba273e94144d458f
jen140 <jen140@users.noreply.github.com>
2021-03-01 21:16:29 +0000

    Create dns_lookup_caa.rb
    dns_lookup_caa.rb

---
+++ lib/tasks/dns_lookup_caa.rb
@@ -0,0 +1,40 @@
+module Intrigue
+module Task
+class DnsLookupCAA < BaseTask
+
+  include Intrigue::Task::Dns
+
+  def self.metadata
+    {
+      :name => "dns_lookup_caa",
+      :pretty_name => "DNS CAA Lookup",
+      :authors => ["jen140"],
+      :description => "Look up the CAA records of the given DNS record.",
+      :references => [],
+      :type => "discovery",
+      :passive => true,
+      :allowed_types => ["Domain"],
+      :example_entities => [{"type" => "Domain", "details" => {"name" => "intrigue.io"}}],
+      :allowed_options => [ ],
+      :created_types => ["tag","host"]
+    }
+  end
+
+  def run
+    super
+
+    domain_name = _get_entity_name
+
+    _log "Running CAA lookup on #{domain_name}"
+
+    res_answer = collect_caa_records domain_name
+
+    # If we got a success to the query.
+    if res_answer
+      _set_entity_detail "caa_records", res_anwer.answer
+    end
+  end
+
+end
+end
+end
