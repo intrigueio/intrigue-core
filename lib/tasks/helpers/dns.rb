73cf29902abc5e12c6adbc5235dd9d60d5847df7
jen140 <jen140@users.noreply.github.com>
2021-03-04 20:09:31 +0000

    Update dns.rb

--- lib/tasks/helpers/dns.rb
+++ lib/tasks/helpers/dns.rb
@@ -390,6 +390,73 @@ module Dns
 
   mx_records
   end
+  
+  class Resolv::DNS::Resource::IN::CAA < Resolv::DNS::Resource
+    TypeValue = 257
+    ClassValue = IN::ClassValue
+    ClassHash[[TypeValue, ClassValue]] = self
+
+    def initialize(flags, tag, value)
+    # https://tools.ietf.org/html/rfc8659#section-4.1
+    #    +0-1-2-3-4-5-6-7-|0-1-2-3-4-5-6-7-|
+    #    | Flags          | Tag Length = n |
+    #    +----------------|----------------+...+---------------+
+    #    | Tag char 0     | Tag char 1     |...| Tag char n-1  |
+    #    +----------------|----------------+...+---------------+
+    #    +----------------|----------------+.....+----------------+
+    #    | Value byte 0   | Value byte 1   |.....| Value byte m-1 |
+    #    +----------------|----------------+.....+----------------+
+    @flags = flags
+    @tag = tag
+    @value = value
+    end
+
+    ##
+    # Critical Flag
+
+    attr_reader :flags
+
+    ##
+    # Property identifier
+
+    attr_reader :tag
+
+    ##
+    # A sequence of octets representing the Property Value
+
+    attr_reader :value
+
+    def encode_rdata(msg)
+    msg.put_bytes(@flags)
+    msg.put_string(@tag)
+    msg.put_bytes(@value)
+    end
+
+    def self.decode_rdata(msg)
+    flags = msg.get_bytes(1)
+    tag = msg.get_string
+    value = msg.get_bytes
+    new(flags, tag, value)
+    end
+  end
+
+  def collect_caa_records(lookup_name)
+    _log "Collecting CAA records"
+    response = resolve(lookup_name, [Resolv::DNS::Resource::IN::CAA])
+    return [] unless response && !response.empty?
+    caa_records = []
+    response.each do |r|
+      r["lookup_details"].each do |record|
+        next unless record["response_record_type"] == "CAA"
+        caa_records << {
+          "flag" => record["response_record_data"]["flag"],
+          "tag" => record["response_record_data"]["tag"],
+          "host" => "#{record["response_record_data"]["value"]}" }
+      end
+    end
+
+  caa_records
+  end
 
   def collect_spf_details(lookup_name)
     _log "Collecting SPF records"
