022151fec45342b2002bc5a47237159b8881b380
m-q-t <maxim@cyberbullies.net>
2021-04-29 11:17:34 -0700

    rubocop auto clean up ec2 gather task

--- lib/tasks/aws_ec2_gather_instances.rb
+++ lib/tasks/aws_ec2_gather_instances.rb
@@ -1,64 +1,61 @@
 module Intrigue
-module Task
-class AwsEc2GatherInstances < BaseTask
-
-  def self.metadata
-    {
-      :name => "aws_ec2_gather_instances",
-      :pretty_name => "AWS EC2 Gather Instances",
-      :authors => ["jcran"],
-      :description => "This task enumerates machines running under an (authenticated) EC2 account.",
-      :references => [],
-      :type => "discovery",
-      :passive => true,
-      :allowed_types => ["AwsCredential"],
-      :example_entities => [{"type" => "String", "details" => {"name" => "intrigue"}}],
-      :allowed_options => [
-        {:name => "region", :regex => "alpha_numeric", :default => "us-east-1" }
-      ],
-      :created_types => ["IpAddress"]
-    }
-  end
-
-  ## Default method, subclasses must override this
-  def run
-    super
-
-    # Get the AWS Credentials
-    aws_access_key = @entity.details["hidden_access_id"]
-    aws_secret_key = @entity.details["hidden_secret_key"]
-
-    # Get the region
-    aws_region = _get_option "region"
-
-    begin
-      # Connect to AWS using fog ...
-      # TODO... prob want to remove the fog dep
-      ec2 = Fog::AWS::Compute.new({
-        :aws_access_key_id => aws_access_key,
-        :aws_secret_access_key => aws_secret_key,
-        :provider => "AWS",
-        :region => aws_region })
-
-      # "organization" => "#{ec2.arn.split(":")[4]}",
-      # Pull each server out and create an IPAddress
-      ec2.servers.each do |s|
-
-        _create_entity "IpAddress", {
-            "name" => "#{s.public_ip_address}",
-            "region" => "#{aws_region}",
-            "private_dns_name" => "#{s.private_dns_name}",
-            "private_ip_address" => "#{s.private_ip_address}",
-            "public_dns_name" => "#{s.dns_name}",
-            "ec2" => s.attributes.to_h
+  module Task
+    class AwsEc2GatherInstances < BaseTask
+      def self.metadata
+        {
+          name: 'aws_ec2_gather_instances',
+          pretty_name: 'AWS EC2 Gather Instances',
+          authors: ['jcran'],
+          description: 'This task enumerates machines running under an (authenticated) EC2 account.',
+          references: [],
+          type: 'discovery',
+          passive: true,
+          allowed_types: ['AwsCredential'],
+          example_entities: [{ 'type' => 'String', 'details' => { 'name' => 'intrigue' } }],
+          allowed_options: [
+            { name: 'region', regex: 'alpha_numeric', default: 'us-east-1' }
+          ],
+          created_types: ['IpAddress']
         }
       end
-    rescue Fog::Compute::AWS::Error => e
-      _log_error "Error: #{e}"
+
+      ## Default method, subclasses must override this
+      def run
+        super
+
+        # Get the AWS Credentials
+        aws_access_key = @entity.details['hidden_access_id']
+        aws_secret_key = @entity.details['hidden_secret_key']
+
+        # Get the region
+        aws_region = _get_option 'region'
+
+        begin
+          # Connect to AWS using fog ...
+          # TODO... prob want to remove the fog dep
+          ec2 = Fog::AWS::Compute.new({
+                                        aws_access_key_id: aws_access_key,
+                                        aws_secret_access_key: aws_secret_key,
+                                        provider: 'AWS',
+                                        region: aws_region
+                                      })
+
+          # "organization" => "#{ec2.arn.split(":")[4]}",
+          # Pull each server out and create an IPAddress
+          ec2.servers.each do |s|
+            _create_entity 'IpAddress', {
+              'name' => s.public_ip_address.to_s,
+              'region' => aws_region.to_s,
+              'private_dns_name' => s.private_dns_name.to_s,
+              'private_ip_address' => s.private_ip_address.to_s,
+              'public_dns_name' => s.dns_name.to_s,
+              'ec2' => s.attributes.to_h
+            }
+          end
+        rescue Fog::Compute::AWS::Error => e
+          _log_error "Error: #{e}"
+        end
+      end
     end
-
   end
-
-end
-end
 end
