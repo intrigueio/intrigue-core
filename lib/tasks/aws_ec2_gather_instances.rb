4a2510c8599ddc0959fc3fa8d10c7ad290d01e26
m-q-t <maxim@cyberbullies.net>
2021-05-11 19:52:07 -0700

    need to fix possible? bug with ec2 instance enrichment; test with ipv6

--- lib/tasks/aws_ec2_gather_instances.rb
+++ lib/tasks/aws_ec2_gather_instances.rb
@@ -10,8 +10,8 @@ module Intrigue
           references: [],
           type: 'discovery',
           passive: true,
-          allowed_types: ['AwsCredential'],
-          example_entities: [{ 'type' => 'String', 'details' => { 'name' => 'AKIAOKZ52BOMZQ9WNUZ2:zc3w4zt5TU0/ItZ+OlzvaIdytJEz352fzH21ZonO9' } }],
+          allowed_types: ['String'],
+          example_entities: [{ 'type' => 'String', 'details' => { 'name' => '__IGNORE__', 'default' => '__IGNORE__' } }],
           allowed_options: [
             { name: 'region', regex: 'alpha_numeric', default: 'us-east-1' }
           ],
@@ -24,8 +24,8 @@ module Intrigue
         super
 
         # Get the AWS Credentials
-        aws_access_key = @entity.details['hidden_access_id']
-        aws_secret_key = @entity.details['hidden_secret_key']
+        aws_access_key = _get_task_config('aws_access_key_id')
+        aws_secret_key = _get_task_config('aws_secret_access_key')
         aws_region = _get_option 'region'
 
         ec2 = Aws::EC2::Resource.new(region: aws_region, access_key_id: aws_access_key, secret_access_key: aws_secret_key)
@@ -45,22 +45,25 @@ module Intrigue
         end
 
         unless instances.count.positive?
-          _log "No EC2 instances were discovered in #{aws_region}!"
+          _log_error "No EC2 instances were discovered in #{aws_region}!"
           return nil
         end
 
         create_entities instances
       end
 
+      # need to test with ipv6
       def create_entities(ec2_instances)
         ec2_instances.each do |instance|
-          _create_entity 'IpAddress', {
-            'name' => instance.public_ip_address.nil? ? '0.0.0.0' : instance.public_ip_address,
+
+          _create_entity 'AwsEC2Instance', {
+            'name' => instance.public_dns_name.empty? ? instance.private_dns_name : instance.public_dns_name,
             'region' => instance.placement.availability_zone.chop,
             'availability_zone' => instance.placement.availability_zone,
+            'public_ip_address' => instance.public_ip_address,
             'private_dns_name' => instance.private_dns_name,
             'private_ip_address' => instance.private_ip_address,
-            'public_dns_name' => instance.public_ip_address
+            'public_dns_name' => instance.public_dns_name
           }
         end
       end
