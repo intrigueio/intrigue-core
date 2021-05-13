633349d47d61c9d1dd77c5b4961e919ef4fed6ef
Shpend Kurtishaj <shpendk13@gmail.com>
2021-05-14 00:23:54 +0200

    move all patterns to json file

--- lib/entities/unique_token.rb
+++ lib/entities/unique_token.rb
@@ -17,110 +17,30 @@ class UniqueToken < Intrigue::Core::Model::Entity
   # Also Handy: https://raw.githubusercontent.com/random-robbie/keywords/master/keywords.txt
   # Also Handy: https://gist.github.com/nullenc0de/2473b1d49dfe4b94088304d542eb3760
   def self.supported_token_types
-
-    #
-    # 13e49d785d8d4f828038b6136f3b48ba
-    # aizasyddwkzzfc5vrgmgao4skp_2kse48xz5tyg
-    #
-
-    tokens = [
-      {
-        "provider": "aws_access_key",
-        "regex": /^(A3T[A-Z0-9]|AKIA|AGPA|AROA|AIPA|ANPA|ANVA|ASIA)[A-Za-z0-9_]{16}$/i,
-        "matcher": /((A3T[A-Z0-9]|AKIA|AGPA|AROA|AIPA|ANPA|ANVA|ASIA)[A-Za-z0-9_]{16})/i,
-        "sensitive": true
-      },
-      {
-        "provider": "amazon_mws",
-        "regex": /^amzn\\.mws\\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i,
-        "matcher": /(amzn\\.mws\\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/i,
-        "sensitive": true
-      },
-      {
-        "provider": "google_adsense",
-        "regex": /^pub-\d+$/i,
-        "matcher": /(pub-\d+)/i,
-        "sensitive": false
-      },
-      {
-        "provider": "google_analytics",
-        "regex": /^UA-[\d\-]+$/i,
-        "matcher": /(UA-[\d\-]+)/i,
-        "sensitive": false
-      },
-      {
-        "provider": "google_api",
-        "regex": /^AIza[0-9A-Za-z\\-_]{35}$/i,
-        "matcher": /[\"\'\=](AIza[0-9A-Za-z\\-_]{35})/i,
-        "sensitive": true
-      },
-      {
-        "provider": "http_user",
-        "regex": /^[A-Za-z0-9\-_:\.~]+@.*$/i,
-        "matcher": /[ftp|ftps|http|https]:\/\/([A-Za-z0-9\-_\.~]+)[@]/i,
-        "sensitive": true
-      },
-      {
-        "provider": "http_user_pass",
-        "regex": /^[A-Za-z0-9\-_:\.~]+:[A-Za-z0-9\-_:\.~]+$/i,
-        "matcher": /[ftp|ftps|http|https]:\/\/([A-Za-z0-9\-_\.~]+:[A-Za-z0-9\-_\.~]+)[@]/i,
-        "sensitive": true
-      },
-      {
-        "provider": "hotjar",
-        "regex": /^[\d+]$/,
-        "matcher": /_hjSettings=\{hjid:([\d+]),/i,
-        "sensitive": false
-      },
-      {
-        "provider": "intercom",
-        "regex": /^[\w\d]{0,8}$/,
-        "matcher": /app_id: \"([\w\d]{0,8})\"\,/i,
-        "sensitive": false
-      },
-      {
-        "provider": "mailchimp",
-        "regex": /^[0-9a-f]{32}-us[0-9]{1,2}$/,
-        "matcher": /[\"\'\=]([0-9a-f]{32}-us[0-9]{1,2})/i,
-        "sensitive": true
-      },
-      {
-        "provider": "slack_person",
-        "regex": /^xoxp-[0-9A-Za-z\\-]{72}$/,
-        "matcher": /[\"\'\=](xoxp-[0-9A-Za-z\\-]{72})/i,
-        "sensitive": true
-      },
-      {
-        "provider": "slack_bot",
-        "regex": /^xoxb-[0-9A-Za-z\\-]{51}$/,
-        "matcher": /[\"\'\=](xoxp-[0-9A-Za-z\\-]{51})/i,
-        "sensitive": true
-      }
-    ]
-
-    addtl_tokens = JSON.parse(File.read(
+    tokens = JSON.parse(File.read(
       "#{$intrigue_basedir}/data/token_patterns.json"))
 
-  # return both
-  tokens.concat(addtl_tokens)
+    # return
+    return tokens
   end
 
   def validate_entity
 
     # check that our regex for the hash matches
-    supported_type = self.class.supported_token_types.find{ |p|
-      regex = p["regex"] || p[:regex]
-      regex = Regexp.new(regex) unless regex.kind_of?(Regexp)
-      name.match(regex)
-    }
+    #supported_type = self.class.supported_token_types.find{ |p|
+    #  regex = p["regex"] || p[:regex]
+    #  regex = Regexp.new(regex) unless regex.kind_of?(Regexp)
+    #  name.match(regex)
+    #}
 
-    if supported_type
-      # set the detail here
-      set_detail("provider", supported_type[:provider] || supported_type["provider"] )
-      set_detail("sensitive", supported_type[:sensitive] || supported_type["sensitive"] )
-    end
+    #if supported_type
+    #  # set the detail here
+    #  set_detail("provider", supported_type[:provider] || supported_type["provider"] )
+    #  set_detail("sensitive", supported_type[:sensitive] || supported_type["sensitive"] )
+    #end
 
-  !supported_type.nil?
+  #!supported_type.nil?
+  true
   end
 
   def scoped?
