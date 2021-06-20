7c75943012c2ceb17b3d31252954e5859b43b340
m-q-t <maxim@cyberbullies.net>
2021-06-19 22:08:05 -0700

    rewrite s3 bruteforce buckets

---
+++ lib/tasks/aws_s3_bruteforce_buckets.rb
@@ -0,0 +1,96 @@
+module Intrigue
+  module Task
+    class AwsS3Brute < BaseTask
+      include Intrigue::Task::Web
+
+      def self.metadata
+        {
+          name: 'aws_s3_bruteforce_buckets',
+          pretty_name: 'AWS S3 Bruteforce Buckets',
+          authors: ['jcran', 'maxim'],
+          description: 'This task takes any keywords (including domains) and uses them to create permutations which are then bruteforced to confirm if a bucket with that name exists.',
+          references: [],
+          type: 'discovery',
+          passive: true,
+          allowed_types: ['DnsRecord', 'Domain', 'IpAddress', 'Organization', 'String'],
+          example_entities: [
+            { 'type' => 'String', 'details' => { 'name' => 'test' } }
+          ],
+          allowed_options: [
+            { name: 'additional_permutation_wordlist', regex: 'alpha_numeric_list', default: [] },
+            { name: 'additional_keywords', regex: 'alpha_numeric_list', default: [] } # these can only be alphanumerical due to regex
+          ],
+          created_types: ['AwsS3Bucket']
+        }
+      end
+
+      ## Default method, subclasses must override this
+      def run
+        super
+        # create permutations
+        additional_keywords = _get_option('additional_keywords').split(',')
+        additional_keywords << _get_entity_name
+
+        permutations = additional_keywords.map { |k| generate_permutations(k) } # generate permutations
+        permutations = permutations.flatten # turn 2d into 1d array
+        permutations << additional_keywords.flatten # add the original keywords to the wordlist as well just in case
+
+        valid_buckets = bruteforce_buckets(permutations)
+        _log "Found #{valid_buckets.size} valid buckets."
+        return if valid_buckets.empty?
+
+        valid_buckets.each { |v| create_entity(v) }
+      end
+
+      # generate permutations using the keywords provided along with the additional permutations wordlist
+      def generate_permutations(keyword)
+        _log "Generating permutations for keyword: #{keyword}"
+        permutations = []
+        patterns = ['.', '-', '']
+
+        words = ['backup', 'backups', 'dev', 'development', 'eng', 'engineering', 'old', 'prod', 'qa', 'stage', 'staging', 'test', 'testing', 'marketing', 'web']
+        words = (words << _get_option('additional_permutation_wordlist').split(',')).flatten.uniq # append the user's provided permutations to our default ones
+
+        patterns.each do |pattern|
+          words.each do |word|
+            permutations << "#{keyword}#{pattern}#{word}"
+            permutations << "#{word}#{pattern}#{keyword}"
+          end
+        end
+
+        permutations
+      end
+
+      def bruteforce_buckets(wordlist)
+        buckets = []
+        workers = (0...20).map do
+          check = bucket_exists?(wordlist, buckets)
+          [check]
+        end
+        workers.flatten.map(&:join)
+        buckets
+      end
+
+      def bucket_exists?(input_q, output_q)
+        t = Thread.new do
+          until input_q.empty?
+            while bucket = input_q.shift
+              region = http_request(:get, "https://#{bucket}.s3.amazonaws.com/").headers['x-amz-bucket-region']
+              output_q << bucket if region
+            end
+          end
+        end
+        t
+      end
+
+      def create_entity(bucket)
+        _create_entity 'AwsS3Bucket', {
+          'name' => bucket, # use the new virtual host path since path style will be deprecated,
+          'bucket_name' => bucket,
+          'bucket_uri' => "#{bucket}.s3.amazonaws.com"
+        }
+      end
+
+    end
+  end
+end
