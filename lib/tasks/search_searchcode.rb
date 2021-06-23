82a1b0f2b2f74659ed3362bdb0964f3605cdcf18
m-q-t <maxim@cyberbullies.net>
2021-06-23 16:53:54 -0700

    thread searchcode

--- lib/tasks/search_searchcode.rb
+++ lib/tasks/search_searchcode.rb
@@ -21,34 +21,56 @@ module Intrigue
         super
 
         keyword = _get_entity_name
-        results = search(keyword)
+        pages = return_amount_of_pages(keyword)
+        return if pages.nil?
+
+        _log_good "There are #{pages + 1} pages matching." # debug
+        pages = pages > 50 ? 49 : pages  # api only allows 49 pages max; if greater than 50 default to 49
+        results = threaded_lookups(keyword, pages)
 
         _log "Obtained #{results.size} results."
       end
 
-      def search(key)
-        found_results = []
+      def return_amount_of_pages(query)
+        response = http_get_body("https://searchcode.com/api/codesearch_I?q=#{query}")
+        output = JSON.parse(response)
+        total = output['total']
+        return if total.nil?
 
-        begin
-          page = 0
-          while page <= 49 # max 49 pages
-            response = http_get_body "https://searchcode.com/api/codesearch_I/?q=#{key}&p=#{page}"
-            output = JSON.parse(response)
-            results = output['results']
+        # take total and divide by 100 as 100 results per page & round up
+        (total / 100.to_f).ceil
+      rescue JSON::ParserError
+        _log_error 'Error Parsing JSON'
+      end
 
-            break if results.nil?
+      def threaded_lookups(query, pages)
+        input =  0.step(pages).map { |p| "https://searchcode.com/api/codesearch_I?q=#{query}&p=#{p}&per_page=100" }
+        output = []
 
-            results.each do |r|
-              found_results << [r['repo'], r['lines'], r['filename']]
-            end
-
-            page += 1
-          end
-        rescue JSON::ParserError
-          _log_error 'Error parsing JSON.'
+        workers = (0...20).map do
+          search = search_page(input, output)
+          [search]
         end
+        workers.flatten.map(&:join)
+        output
+      end
 
-        found_results
+      def search_page(input_q, output_q)
+        t = Thread.new do
+          until input_q.empty?
+            while url = input_q.shift
+              begin
+                _log "Attempting #{url}" # debug
+                response = http_get_body url
+                results_json = JSON.parse(response)
+              rescue JSON::ParserError
+                next
+              end
+              output_q << results_json['results']
+            end
+          end
+        end
+        t
       end
 
     end
