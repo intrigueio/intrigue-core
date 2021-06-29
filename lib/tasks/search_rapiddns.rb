16632fdfec72c76f2f7b9ecef1ee5d2db2675295
m-q-t <maxim@cyberbullies.net>
2021-06-29 16:54:37 -0700

    bug fixed was due to timeout

--- lib/tasks/search_rapiddns.rb
+++ lib/tasks/search_rapiddns.rb
@@ -24,14 +24,19 @@ module Intrigue
         domain = _get_entity_name
 
         _log "Retrieving results for #{domain} from RapidDNS."
-        response = http_get_body("https://rapiddns.io/subdomain/#{domain}?full=1#result")
 
-        doc = Nokogiri::HTML(response)
-        subdomains = doc.xpath('//*[@id="table"]').search('tr').map { |tr| tr.content.split("\n")[2] }
-        subdomains = subdomains.drop(1).uniq # the first element will always be 'Domain' whether results returned or not
+        # large sites take longer than 10 seconds (default timeout of http_get_body) to return
+        response = http_request(:get, "https://rapiddns.io/subdomain/#{domain}?full=1#result", nil, {}, nil, true, 45).body
+        subdomains = parse_response(response)
 
         _log "Retrieved a total of #{subdomains.size} results."
         subdomains.each { |s| create_dns_entity_from_string(s) } unless subdomains.empty?
+      end
+
+      def parse_response(http_response)
+        doc = Nokogiri::HTML(http_response)
+        parsed_subdomains = doc.xpath('//*[@id="table"]').search('tr').map { |tr| tr.content.split("\n")[2] }
+        parsed_subdomains.drop(1).uniq # the first element will always be 'Domain' whether results returned or not
       end
 
     end
