8ea899a49841e05723d71774fe1e900b03151269
Shpend Kurtishaj <shpendk13@gmail.com>
2021-07-09 21:30:41 +0200

    cve-2021-30117

--- lib/checks/kaseya_cve_2021_30117.rb
+++ lib/checks/kaseya_cve_2021_30117.rb
@@ -2,24 +2,24 @@
 module Intrigue
 
   module Issue
-    class KaseyaCve202130116 < BaseIssue
+    class KaseyaCve202130117 < BaseIssue
       def self.generate(instance_details={})
       {
         added: "2021-07-09",
-        name: "kaseya_cve_2021_30116",
-        pretty_name: "Kaseya Credential Disclosure (CVE-2021-30116)",
+        name: "kaseya_cve_2021_30117",
+        pretty_name: "Kaseya Credential Disclosure (CVE-2021-30117)",
         identifiers: [
-          { type: "CVE", name: "CVE-2021-30116" }
+          { type: "CVE", name: "CVE-2021-30117" }
         ],
         severity: 1,
         category: "vulnerability",
         status: "potential",
-        description: "Kaseya VSA before 9.5.7 allows credential disclosure, as exploited in the wild in July 2021.",
+        description: "SQL injection exists in Kaseya VSA before 9.5.6.",
         affected_software: [ 
           { :vendor => "Kaseya", :product => "Virtual System Administrator" }
         ],
         references: [
-          { type: "description", uri: "https://nvd.nist.gov/vuln/detail/CVE-2021-30116" },
+          { type: "description", uri: "https://nvd.nist.gov/vuln/detail/CVE-2021-30117" },
           { type: "description", uri: "https://helpdesk.kaseya.com/hc/en-gb/articles/4403440684689-Important-Notice-July-2nd-2021" }
         ],
         authors: ["shpendk"]
@@ -29,7 +29,7 @@ module Intrigue
   end
 
   module Task
-    class KaseyaCve202130116 < BaseCheck 
+    class KaseyaCve202130117 < BaseCheck 
     def self.check_metadata
       {
         allowed_types: ["Uri"]
@@ -47,7 +47,7 @@ module Intrigue
       return false unless version
 
       # if its vulnerable, return some proof
-      if compare_versions_by_operator(version, "9.5.7" , "<")
+      if compare_versions_by_operator(version, "9.5.6" , "<")
         return "Asset is vulnerable based on fingerprinted version #{version}"
       end
     end
