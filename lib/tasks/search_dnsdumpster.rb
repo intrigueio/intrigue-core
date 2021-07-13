1fe8f89b0ec78b0a047f0e9932cc09fa9d660263
Xiao-Lei Xiao <thechowdud@gmail.com>
2021-07-13 21:21:56 +0100

    Applies formatting on the code

--- lib/tasks/search_dnsdumpster.rb
+++ lib/tasks/search_dnsdumpster.rb
@@ -1,48 +1,44 @@
 module Intrigue
-module Task
-class SearchDNSdumpster < BaseTask
-    def self.metadata
-    {
-        :name => "search_dnsdumpster",
-        :pretty_name => "Search DNSdumpster",
-        :authors => ["Xiao-Lei Xiao"],
-        :description => "This task utilises the domain research tool DNSdumpster to research, find & lookup dns records. ",
-        :references => ["https://dnsdumpster.com/"],
-        :type => "discovery",
-        :passive => true,
-        :allowed_types => ["Domain"],
-        :example_entities => [
-        {"type" => "Domain", "details" => {"name" => "intrigue.io"}},
-        ],
-        :allowed_options => [],
-        :created_types => ['Domain', 'DnsRecord']
-    }
-    end
+  module Task
+    class SearchDNSdumpster < BaseTask
+      def self.metadata
+        {
+          name: 'search_dnsdumpster',
+          pretty_name: 'Search DNSdumpster',
+          authors: ['Xiao-Lei Xiao'],
+          description: 'This task utilises the domain research tool DNSdumpster to research, find & lookup dns records. ',
+          references: ['https://dnsdumpster.com/'],
+          type: 'discovery',
+          passive: true,
+          allowed_types: ['Domain'],
+          example_entities: [
+            { 'type' => 'Domain', 'details' => { 'name' => 'intrigue.io' } }
+          ],
+          allowed_options: [],
+          created_types: ['Domain', 'DnsRecord']
+        }
+      end
 
-    ## Default method, subclasses must override this
-    def run
+      ## Default method, subclasses must override this
+      def run
         super
-  
+
         entity_name = _get_entity_name
-  
-        begin
-            response = http_request(:get, "https://dnsdumpster.com/", nil, {})
-            cookie = response.headers["Set-Cookie"].match(/(.*?;){1}/)
-            token = cookie.to_s.scan(/=(.+)[,;]$/)[0][0]
 
-            response = http_post("https://dnsdumpster.com/", "csrfmiddlewaretoken=#{token}&targetip=#{entity_name}", {
-                "Referer" =>  "https://dnsdumpster.com/" ,
-                "Cookie" => "#{cookie}" 
-            })
-           
-            doc = Nokogiri::HTML(response.body_utf8)
-            subdomains = doc.css('table')[0].search('tr').map { |tr| tr.content.delete!("\n").split(" ")[0] }
-            
-            subdomains.each { |s| create_dns_entity_from_string(s) } unless subdomains.empty?
-          end
-  
-    end #end run
+        response = http_request(:get, 'https://dnsdumpster.com/', nil, {})
+        cookie = response.headers['Set-Cookie'].match(/(.*?;){1}/)
+        token = cookie.to_s.scan(/=(.+)[,;]$/)[0][0]
 
-end
-end
+        response = http_post('https://dnsdumpster.com/', "csrfmiddlewaretoken=#{token}&targetip=#{entity_name}", {
+                               'Referer' => 'https://dnsdumpster.com/',
+                               'Cookie' => cookie.to_s
+                             })
+
+        doc = Nokogiri::HTML(response.body_utf8)
+        subdomains = doc.css('table')[0].search('tr').map { |tr| tr.content.delete!("\n").split(' ')[0] }
+
+        subdomains.each { |s| create_dns_entity_from_string(s) } unless subdomains.empty?
+      end
+    end
+  end
 end
