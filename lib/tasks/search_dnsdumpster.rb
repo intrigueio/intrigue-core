83d808cd6d3e1cbc25456f62e220e80d8ceb6340
Xiao-Lei Xiao <thechowdud@gmail.com>
2021-07-17 00:29:11 +0100

    Adds Logging to Task and Handling for timeout

--- lib/tasks/search_dnsdumpster.rb
+++ lib/tasks/search_dnsdumpster.rb
@@ -29,13 +29,27 @@ module Intrigue
         cookie = response.headers['Set-Cookie'].match(/(.*?;){1}/)
         token = cookie.to_s.scan(/=(.+)[,;]$/)[0][0]
 
-        response = http_post('https://dnsdumpster.com/', "csrfmiddlewaretoken=#{token}&targetip=#{entity_name}", {
-                               'Referer' => 'https://dnsdumpster.com/',
-                               'Cookie' => cookie.to_s
-                             })
+        response = http_request(:post, 'https://dnsdumpster.com/', nil, {
+                                  'Referer' => 'https://dnsdumpster.com/',
+                                  'Cookie' => cookie.to_s
+                                }, "csrfmiddlewaretoken=#{token}&targetip=#{entity_name}", true, 300)
+
+        if response.return_code == :operation_timedout
+          _log_error 'Request timed out. Try again later.'
+          return
+        end
 
         doc = Nokogiri::HTML(response.body_utf8)
+        table = doc.css('table')[0]
+
+        if table.nil?
+          _log_error "Could not find the DNS table for #{entity_name}"
+          return
+        end
+
         subdomains = doc.css('table')[0].search('tr').map { |tr| tr.content.delete!("\n").split(' ')[0] }
+        
+        _log_good "Found #{subdomains.length} subdomains for #{entity_name}"
 
         subdomains.each { |s| create_dns_entity_from_string(s) } unless subdomains.empty?
       end
