1e2b4f7dee39cbee2ec39125c4f133ea12b9aee4
m-q-t <maxim@cyberbullies.net>
2021-07-30 16:04:55 -0700

    search grayhat warfare refactor + include new entities/helpers

--- lib/tasks/search_grayhat_warfare.rb
+++ lib/tasks/search_grayhat_warfare.rb
@@ -1,68 +1,80 @@
 module Intrigue
   module Task
     class SearchGrayhatWarfare < BaseTask
-    
-    def self.metadata
-      {
-        name: 'search_grayhat_warfare',
-        pretty_name: 'Search Grayhat Warfare',
-        authors: ['jcran', 'Anas Ben Salah'],
-        description: 'This task hits the Grayhat Warfare API and finds buckets.',
-        references: [],
-        type: 'discovery',
-        passive: true,
-        allowed_types: ['DnsRecord', 'Domain', 'String', 'UniqueKeyword'],
-        example_entities: [{ 'type' => 'String', 'details' => { 'name' => 'intrigue.io' } }],
-        allowed_options: [
-          { name: 'max_buckets', regex: 'integer', default: 100 }
-        ],
-        created_types: ['AwsS3Bucket', 'AzureBlob']
-      }
-    end
+      def self.metadata
+        {
+          name: 'search_grayhat_warfare',
+          pretty_name: 'Search Grayhat Warfare',
+          authors: ['jcran', 'Anas Ben Salah', 'maxim'],
+          description: 'This task hits the Grayhat Warfare API and finds buckets.',
+          references: [],
+          type: 'discovery',
+          passive: true,
+          allowed_types: ['DnsRecord', 'Domain', 'String', 'UniqueKeyword'],
+          example_entities: [{ 'type' => 'String', 'details' => { 'name' => 'intrigue.io' } }],
+          allowed_options: [
+            { name: 'max_buckets', regex: 'integer', default: 100 }
+          ],
+          created_types: ['AwsS3Bucket', 'AzureStorageAccount']
+        }
+      end
 
-    ## Default method, subclasses must override this
-    def run
-      super
+      ## Default method, subclasses must override this
+      def run
+        super
+        search_uri = construct_api_call
 
-      # Make sure the key is set
-      api_key = _get_task_config('grayhat_warfare_api_key')
-      max_buckets = _get_option('max_buckets')
+        output = return_grayhat_response(search_uri)
+        return if output.nil?
 
-      search_string = _get_entity_name
-      search_uri = "https://buckets.grayhatwarfare.com/api/v1/buckets/0/#{max_buckets}?access_token=#{api_key}&keywords=#{search_string}"
+        _log_good "Obtained #{output.size} results!"
+        iterate_results(output) unless output.empty?
+      end
 
-      begin
+      def create_s3_bucket(s3)
+        _create_entity 'AwsS3Bucket', {
+          'name' => extract_bucket_name_from_string(s3['bucket']),
+          'bucket_name' => extract_bucket_name_from_string(s3['bucket']),
+          'bucket_uri' => "https://#{s3['bucket']}",
+          'objects' => s3['fileCount']
+        }
+      end
 
-        # get it, make sure we don't have an empty response
-        output = JSON.parse(http_get_body(search_uri))
-        _log 'Unable to find any results.' unless output
-        return unless output
+      def create_azure_storage_account(azure)
+        _create_entity 'AzureStorageAccount', {
+          'name' => extract_storage_account_from_string(azure['bucket']),
+          'storage_name' => extract_storage_account_from_string(azure['bucket']),
+          'uri' => azure['bucket'],
+          'fileCount' => azure['fileCount'],
+          'containers' => [azure['container']]
+        }
+      end
 
-        output['buckets'].each do |b|
-          # {"id"=>1096, "bucket"=>"example.amazonaws.com", "fileCount"=>11, "type"=>"aws"}
-          # {"id"=>137, "bucket"=>"abteststore.blob.core.windows.net", "fileCount"=>2, "type"=>"azure", "container"=>"files"}
-          if b['type'] == 'aws'
-            _create_entity 'AwsS3Bucket', {
-              'name' => extract_bucket_name_from_uri(b['bucket']),
-              'bucket_name' => extract_bucket_name_from_uri(b['bucket']),
-              'bucket_uri' => "https://#{b["bucket"]}",
-              'objects' =>  b['fileCount'],
-            }
-          elsif b['type'] == 'azure'
-            _create_entity 'AzureBlob', {
-              'name' => "https://#{b["bucket"]}",
-              'uri' => "https://#{b["bucket"]}",
-              'fileCount' =>  b['fileCount'],
-              'type' =>  b['type']
-            }
-          else
-            _log 'undefined storage type!'
+      def iterate_results(results)
+        results['buckets'].each do |b|
+          case b['type']
+          when 'aws'
+            create_s3_bucket(b)
+          when 'azure'
+            create_azure_storage_account(b)
           end
         end
+      end
+
+      def return_grayhat_response(uri)
+        JSON.parse(http_get_body(uri))
       rescue JSON::ParserError => e
         _log_error "Unable to parse: #{e}"
       end
-    end
+
+      def construct_api_call
+        api_key = _get_task_config('grayhat_warfare_api_key')
+        max_buckets = _get_option('max_buckets')
+
+        search_string = _get_entity_name
+        "https://buckets.grayhatwarfare.com/api/v1/buckets/0/#{max_buckets}?access_token=#{api_key}&keywords=#{search_string}"
+      end
+
     end
   end
 end
