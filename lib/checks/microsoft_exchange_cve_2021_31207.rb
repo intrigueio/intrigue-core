f15da3b5a80ec989d1a57df6b4a504723c6b2115
Shpend Kurtishaj <shpendk13@gmail.com>
2021-08-09 16:31:18 +0200

    cve-2021-31207

--- lib/checks/microsoft_exchange_cve_2021_31207.rb
+++ lib/checks/microsoft_exchange_cve_2021_31207.rb
@@ -2,19 +2,19 @@
 module Intrigue
 
     module Issue
-      class MicrosoftExchangeCve202131206 < BaseIssue
+      class MicrosoftExchangeCve202131207 < BaseIssue
         def self.generate(instance_details={})
         {
           added: "2021-08-09",
-          name: "microsoft_exchange_cve_2021_31206",
-          pretty_name: "Microsoft Exchange Server Remote Code Execution (CVE-2021-31206)",
+          name: "microsoft_exchange_cve_2021_31207",
+          pretty_name: "Microsoft Exchange Server Security Feature Bypass (CVE-2021-31207)",
           identifiers: [
-            { type: "CVE", name: "CVE-2021-31206" }
+            { type: "CVE", name: "CVE-2021-31207" }
           ],
-          severity: 1,
+          severity: 2,
           status: "potential",
           category: "vulnerability",
-          description: "Microsoft Exchange Server Remote Code Execution Vulnerability.",
+          description: "Microsoft Exchange Server Security Feature Bypass Vulnerability",
           remediation: "Install the latest security update for the specific products.",
           affected_software: [
             { :vendor => "Microsoft", :product => "Exchange Server", :version => "2013"},
@@ -22,10 +22,10 @@ module Intrigue
             { :vendor => "Microsoft", :product => "Exchange Server", :version => "2019"}
           ],
           references: [
-            { type: "description", uri: "https://msrc.microsoft.com/update-guide/vulnerability/CVE-2021-31206" },
-            { type: "description", uri: "https://nvd.nist.gov/vuln/detail/CVE-2021-31206"}
+            { type: "description", uri: "https://msrc.microsoft.com/update-guide/vulnerability/CVE-2021-31207" },
+            { type: "description", uri: "https://nvd.nist.gov/vuln/detail/CVE-2021-31207"}
           ],
-          authors: ["shpendk", "Steven Seeley (mr_me)"]
+          authors: ["shpendk", "Orange Tsai"]
         }.merge!(instance_details)
         end
 
@@ -34,7 +34,7 @@ module Intrigue
 
 
     module Task
-      class MicrosoftExchangeCve202131206 < BaseCheck
+      class MicrosoftExchangeCve202131207 < BaseCheck
         def self.check_metadata
           {
             allowed_types: ["Uri"],
@@ -53,12 +53,12 @@ module Intrigue
             { version: "2013", update: "Cumulative Update 23" },
           
             # 2016
+            { version: "2016", update: "Cumulative Update 19" },
             { version: "2016", update: "Cumulative Update 20" },
-            { version: "2016", update: "Cumulative Update 21" },
 
             # 2019
+            { version: "2019", update: "Cumulative Update 8" },
             { version: "2019", update: "Cumulative Update 9" },
-            { version: "2019", update: "Cumulative Update 10" },
           ]
   
           # get version for product
