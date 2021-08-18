c80c6ab3a145ec53d4ea87300eeb311d6140be97
maxim <maxim@cyberbullies.net>
2021-08-18 13:26:10 -0700

    harden regex to validate github_account entity

--- lib/entities/github_account.rb
+++ lib/entities/github_account.rb
@@ -1,33 +1,31 @@
 module Intrigue
-module Entity
-class GithubAccount < Intrigue::Core::Model::Entity
+  module Entity
+    class GithubAccount < Intrigue::Core::Model::Entity
+      def self.metadata
+        {
+          name: 'GithubAccount',
+          description: 'A Github Account',
+          user_creatable: true,
+          example: 'https://github.com/intrigueio'
+        }
+      end
 
-  def self.metadata
-    {
-      name: "GithubAccount",
-      description: "A Github Account",
-      user_creatable: true,
-      example: "intrigueio"
-    }
+      def validate_entity
+        name.match /^https:\/\/github.com\/[\w\-]{1,39}$/
+      end
+
+      def enrichment_tasks
+        # to enrich github accounts, use enrich/github_account and prepend to array below
+        ['gather_github_repositories']
+      end
+
+      def scoped?
+        return scoped unless scoped.nil?
+        return true if allow_list || project.allow_list_entity?(self)
+        return false if deny_list || project.deny_list_entity?(self)
+
+        true
+      end
+    end
   end
-
-  def validate_entity
-    name.match /^[\d\w]+/
-  end
-
-  def enrichment_tasks
-     # to enrich github accounts, use enrich/github_account and prepend to array below
-    ['gather_github_repositories']
-  end
-
-  def scoped?
-    return scoped unless scoped.nil?
-    return true if self.allow_list || self.project.allow_list_entity?(self)
-    return false if self.deny_list || self.project.deny_list_entity?(self)
-
-  true
-  end
-
-end
-end
 end
