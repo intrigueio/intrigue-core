439745ccd6e14223ad21b35a00334a70833da81e
jcran <jcran@intrigue.io>
2021-08-30 14:42:34 -0600

    hook up rdp, smb, snmp and (fix) database issues

--- lib/issues/exposed_snmp_service.rb
+++ lib/issues/exposed_snmp_service.rb
@@ -1,16 +1,16 @@
 module Intrigue
   module Issue
-  class ExposedDatabase < BaseIssue
+  class ExposedSnmpService < BaseIssue
 
     def self.generate(instance_details={})
       {
-        added: "2020-01-01",
-        name: "exposed_database",
-        pretty_name: "Exposed Database",
-        severity: 2,
+        added: "2021-08-30",
+        name: "exposed_snmp_service",
+        pretty_name: "Exposed SNMP Service",
+        severity: 3,
         status: "confirmed",
         category: "leak",
-        description: "A database was found exposed.",
+        description: "An SNMP service was found exposed.",
         remediation: "Investigate and determine if this system should be exposed to anonymous users.",
         references: [ # types: description, remediation, detection_rule, exploit, threat_intel
         ],
