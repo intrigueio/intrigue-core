cc843dc1a4b0c2af3c0e44863eba4ff3d3f1876f
Duarte Mateus <duarte.mateus@intrigue.io>
2021-09-14 21:09:06 +0100

    Fixes redirect_chain build

--- lib/tasks/helpers/redirect_chain.rb
+++ lib/tasks/helpers/redirect_chain.rb
@@ -8,18 +8,16 @@ module Intrigue
         max_redirect_count = Intrigue::Core::System::Config.config['redirect_issue_trigger_count'] || 10
 
         # iterate through responses
+
+        _log_debug "Max redirect count: #{max_redirect_count}"
         begin
-
-          _log_debug "Max redirect count: #{max_redirect_count}"
-
           responses.each do |r|
             next unless r || !r[:redirect_chain].empty?
 
             # this is to fix the ordering when saving to the DB.
             # If we change ident to return the hash props as "source" and "destination".
             # We can then remove this code.
-            all_chains << r[:redirect_chain].map { |x|{ 'source' => x[:from].to_s, 'destination' => x[:to].to_s }
-            end
+            all_chains << r[:redirect_chain].map { |x| { 'source' => x[:from].to_s, 'destination' => x[:to].to_s } }
           end
 
           # drop dupes ... keep in mind this is an array of arrays
@@ -36,14 +34,14 @@ module Intrigue
             end
           end
 
-        # TODO ... can we fully remove this?
+          # TODO: ... can we fully remove this?
         rescue StandardError => e
           _log_error "Unable to fetch redirect chain: #{e}"
         end
 
         {
-          chain: redirect_complete_chain,
-          count: all_chains.map{|c| c.count }.inject(0, :+)
+          chain: all_chains,
+          count: all_chains.map { |c| c.count }.inject(0, :+)
         }
       end
     end
