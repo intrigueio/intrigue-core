579787177952407d2044e2c3b24216ed31e0424e
Duarte Mateus <duarte.mateus@intrigue.io>
2021-09-15 12:29:08 +0100

    Fixes vnc_screenshot number comparison.

--- lib/tasks/vnc_screenshot.rb
+++ lib/tasks/vnc_screenshot.rb
@@ -25,7 +25,7 @@ module Intrigue
 
         port = net_service.split(':').last.split('/').first
 
-        unless port.between?(5900,5999)
+        unless port.to_i.between?(5900,5999)
           _log 'Incorrect VNC port! Will not perform certificate extraction.'
           return
         end
