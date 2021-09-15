1de240bc79e8439060eb2cfc53a0c85887eb53c0
Duarte Mateus <duarte.mateus@intrigue.io>
2021-09-15 20:03:30 +0100

    Change to geolocation.rb
    Changes prop name.
    Adds .keep to maxmind folder

--- lib/tasks/helpers/geolocation.rb
+++ lib/tasks/helpers/geolocation.rb
@@ -35,7 +35,7 @@ module Intrigue
           hash[:continent_code] = location.continent.code
           hash[:country] = location.country.name(:en)
           hash[:country_code] = location.country.iso_code
-          hash[:latitute] = location.location.latitude
+          hash[:latitude] = location.location.latitude
           hash[:longitude] = location.location.longitude
           hash[:accuracy_radius] = location.location.accuracy_radius
           hash[:time_zone] = location.location.time_zone
@@ -44,7 +44,7 @@ module Intrigue
           hash[:asn][:name] = asn.to_hash["autonomous_system_organization"] if asn.to_hash["autonomous_system_organization"]
           hash[:asn][:asn] = asn.to_hash["autonomous_system_number"] if asn.to_hash["autonomous_system_number"]
           hash[:asn][:route] = asn.to_hash["network"] if asn.to_hash["network"]
-
+          
           # check if the required keys are present
           required_keys = [:country_code, :country, :longitude, :latitude]
           required_keys.each do |h|
@@ -75,27 +75,6 @@ module Intrigue
       end
 
       private
-
-      def get_config
-        config = _get_system_config('intrigue_global_module_config')['ipdata_api_key']
-
-        if config.nil?
-          _log_error 'Geolocation configuration not set. Unable to proceed with Geolocation task.'
-          return nil
-        end
-
-        if config['value'] == 'CHANGE_ME' || config['value'].nil?
-          _log_error 'Geolocation api key has not been set. Unable to proceed with Geolocation task.'
-          return nil
-        end
-
-        if config['uri'].nil?
-          _log_error 'Geolocation base api url not found. Unable to proceed with Geolocation task.'
-          return nil
-        end
-
-        config
-      end
 
       def build_request(base_url, ip, params)
         URI::HTTPS.build(host: base_url, path: "/#{ip}", query: URI.encode_www_form(params))
