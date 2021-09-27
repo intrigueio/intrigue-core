16996c92b79c14b7d7ae5ccf64d6217b3a4e547a
Shpend Kurtishaj <shpendk13@gmail.com>
2021-09-27 22:24:53 +0200

    bugfix

--- lib/tasks/helpers/services.rb
+++ lib/tasks/helpers/services.rb
@@ -35,9 +35,12 @@ module Services
     # before anything, check if port is open and return nil if not
     # note that we use tcp even for udp ports, because with udp we fire & hope for the best
     # this has been tested and tcp is reliable for detecting open udp ports
-
-    return nil unless Intrigue::Ident::SimpleSocket.connect_tcp(ip_entity.name, port_num)
-    _set_entity_detail("hidden_port_open_confirmed", true)
+    begin
+      return nil unless Intrigue::Ident::SimpleSocket.connect_tcp(ip_entity.name, port_num)
+      _set_entity_detail("hidden_port_open_confirmed", true)
+    rescue Errno::EADDRNOTAVAIL
+      return nil
+    end
 
     # first, save the port details on the ip_entity
     ports = ip_entity.get_detail("ports") || []
