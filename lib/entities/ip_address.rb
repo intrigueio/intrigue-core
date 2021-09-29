41dae8c321185446e8aa39a60872843848c6d300
Shpend Kurtishaj <shpendk13@gmail.com>
2021-09-29 15:52:29 +0200

    remove private ip address logic

--- lib/entities/ip_address.rb
+++ lib/entities/ip_address.rb
@@ -12,38 +12,7 @@ class IpAddress < Intrigue::Core::Model::Entity
   end
 
   def validate_entity
-    scan_internal = (Intrigue::Core::System::Config.config["scan_internal_ips"] == true ) ? true : false
-    
-    # check if valid ipv4 or ipv6
-    unless name.match(ipv4_regex) || name.match(ipv6_regex)
-      return false
-    end
-    
-    # check if localhost
-    if name.match(/^127(?:\.[0-9]+){0,2}\.[0-9]+$|^(?:0*\:)*?:?0*1$/)
-      return false
-    end
-    
-    # check if we should scan internal
-    # https://docs.microfocus.com/NNMi/10.30/Content/Administer/NNMi_Deployment/Advanced_Configurations/Private_IP_Address_Range.htm
-    unless scan_internal
-      # check if we match 172.16.x.x
-      return false if name.match(/(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)/)
-
-      # check if we match 10.x.x.x
-      return false if name.match(/(^10\.)/)
-
-      # check if we match 192.168.x.x
-      return false if name.match(/(^192\.168\.)/)
-
-      # fc00::/7 address block = RFC 4193 Unique Local Addresses (ULA)
-      return false if name.match(/(^fc00\:)/)
-
-      # fec0::/10 address block = deprecated (RFC 3879)
-      return false if name.match(/(^fec0\:)/)
-    end
-
-    return true
+    return name.match(ipv4_regex) || name.match(ipv6_regex)
   end
 
   def detail_string
